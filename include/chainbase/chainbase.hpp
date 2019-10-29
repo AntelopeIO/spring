77897d505a30fcfba0fb2ccfdc4a0effb8f7df9b
Steven Watanabe <steven.watanabe@block.one>
2019-10-29 11:35:17 -0400

    chainbase::undo_state is dead.

--- include/chainbase/chainbase.hpp
+++ include/chainbase/chainbase.hpp
@@ -111,30 +111,6 @@ namespace chainbase {
    template<typename Constructor, typename Allocator> \
    OBJECT_TYPE( Constructor&& c, Allocator&&  ) { c(*this); }
 
-   template< typename value_type >
-   class undo_state
-   {
-      public:
-         typedef typename value_type::id_type                      id_type;
-         typedef allocator< std::pair<const id_type, value_type> > id_value_allocator_type;
-         typedef allocator< id_type >                              id_allocator_type;
-
-         template<typename T>
-         undo_state( allocator<T> al )
-         :old_values( id_value_allocator_type( al.get_segment_manager() ) ),
-          removed_values( id_value_allocator_type( al.get_segment_manager() ) ),
-          new_ids( id_allocator_type( al.get_segment_manager() ) ){}
-
-         typedef boost::interprocess::map< id_type, value_type, std::less<id_type>, id_value_allocator_type >  id_value_type_map;
-         typedef boost::interprocess::set< id_type, std::less<id_type>, id_allocator_type >                    id_type_set;
-
-         id_value_type_map            old_values;
-         id_value_type_map            removed_values;
-         id_type_set                  new_ids;
-         id_type                      old_next_id = 0;
-         int64_t                      revision = 0;
-   };
-
    /**
     * The code we want to implement is this:
     *
