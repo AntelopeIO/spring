8c84f4f503289c7d98e2f7a910f3cace69b4647e
greg7mdp <greg7mdp@gmail.com>
2023-08-17 18:07:30 -0400

    Cleanup include statement in `undo_index.cpp`.

--- test/undo_index.cpp
+++ test/undo_index.cpp
@@ -1,5 +1,5 @@
-#include "chainbase/chainbase.hpp"
 #include <chainbase/undo_index.hpp>
+#include <chainbase/chainbase.hpp>
 
 #include <boost/multi_index/member.hpp>
 #include <boost/multi_index/ordered_index.hpp>
