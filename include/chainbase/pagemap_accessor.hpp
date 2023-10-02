7ae2b7cec75c7fa536cf7dff3d16060dd08980aa
greg7mdp <greg7mdp@gmail.com>
2023-10-02 10:59:24 -0400

    Add another commment.

--- include/chainbase/pagemap_accessor.hpp
+++ include/chainbase/pagemap_accessor.hpp
@@ -63,6 +63,23 @@ public:
       return this->is_marked_dirty(data);
    }
 
+   // /proc/pid/pagemap. This file lets a userspace process find out which physical frame each virtual page
+   // is mapped to. It contains one 64-bit value for each virtual page, containing the following data
+   // (from fs/proc/task_mmu.c, above pagemap_read):
+   // 
+   // Bits 0-54 page frame number (PFN) if present (note: field is zeroed for non-privileged users)
+   // Bits 0-4 swap type if swapped
+   // Bits 5-54 swap offset if swapped
+   // Bit 55 pte is soft-dirty (see Documentation/admin-guide/mm/soft-dirty.rst)
+   // Bit 56 page exclusively mapped (since 4.2)
+   // Bit 57 pte is uffd-wp write-protected (since 5.13) (see Documentation/admin-guide/mm/userfaultfd.rst)
+   // Bits 58-60 zero
+   // Bit 61 page is file-page or shared-anon (since 3.5)
+   // Bit 62 page swapped
+   // Bit 63 page present
+   //
+   // Here we are just checking bit #55 (the soft-dirty bit).
+   // ----------------------------------------------------------------------------------------------------
    bool read(uintptr_t vaddr, std::span<uint64_t> dest_uint64) const {
       if constexpr (!_pagemap_supported)
          return false;
