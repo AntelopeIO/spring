fa7588dfae16474055f88d2657ba71a29dc7848d
Guillaume Babin-Tremblay <gbabint@msn.com>
2024-04-21 19:20:03 +0000

    Improvements to ibc contract bitset

--- unittests/test-contracts/svnn_ibc/bitset.hpp
+++ unittests/test-contracts/svnn_ibc/bitset.hpp
@@ -1,45 +1,51 @@
 using namespace eosio;
 
+
 class bitset {
+    using word_t = uint32_t;
+    static constexpr size_t num_bits = sizeof(word_t) * 8;
+    static size_t round_up(size_t sz) { return (sz + num_bits - 1) / num_bits; }
+
 public:
-    bitset(uint32_t size) 
-        : num_bits(size), data((size + 31) / 32, 0) {}
+    bitset(word_t size) 
+        : num_bits(size), data(round_up(size), 0) {}
     
-   bitset(uint32_t size, const std::vector<uint32_t> raw_bitset)
+   bitset(word_t size, const std::vector<word_t> raw_bitset)
        : num_bits(size), data(raw_bitset) {
-           check(raw_bitset.size() == (size + 31) / 32, "invalid raw bitset size");
+           //check(raw_bitset.size() == (size + 31) / num_bits, "invalid raw bitset size");
+           check(raw_bitset.size() == round_up(size), "invalid raw bitset size");
 
        }
 
     // Set a bit to 1
-    void set(uint32_t index) {
+    void set(word_t index) {
         check_bounds(index);
-        data[index / 32] |= (1UL << (index % 32));
+        data[index / 32] |= (1UL << (index % num_bits));
     }
 
     // Clear a bit (set to 0)
-    void clear(uint32_t index) {
+    void clear(word_t index) {
         check_bounds(index);
-        data[index / 32] &= ~(1UL << (index % 32));
+        data[index / num_bits] &= ~(1UL << (index % num_bits));
     }
 
     // Check if a bit is set
-    bool test(uint32_t index) const {
+    bool test(word_t index) const {
         check_bounds(index);
-        return (data[index / 32] & (1UL << (index % 32))) != 0;
+        return (data[index / num_bits] & (1UL << (index % num_bits))) != 0;
     }
 
     // Size of the bitset
-    uint32_t size() const {
+    word_t size() const {
         return num_bits;
     }
 
 private:
-    uint32_t num_bits;
-    std::vector<uint32_t> data;
+    word_t num_bits;
+    std::vector<word_t> data;
 
     // Check if the index is within bounds
-    void check_bounds(uint32_t index) const {
+    void check_bounds(word_t index) const {
         check(index < num_bits, "bitset index out of bounds");
     }
 };
