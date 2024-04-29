6505e0c8b8d3301eed4b585d3a4da3425bde1789
Kevin Heifner <heifnerk@objectcomputing.com>
2024-04-29 18:41:39 -0500

    Gh-5 Add ordered_diff

---
+++ libraries/libfc/include/fc/container/ordered_diff.hpp
@@ -0,0 +1,99 @@
+#pragma once
+
+#include <vector>
+#include <tuple>
+
+namespace fc {
+
+/**
+ * @class ordered_diff
+ * @brief Provides ablity to generate and apply diff of containers of type T
+ *
+ * Example use:
+ *    std::vector<char> source = { 'a', 'b', 'f', 'c', 'd' };
+ *    std::vector<char> target = { 'b', 'f', 'c', 'd', 'e', 'h' };
+ *    ordered_diff<char>::diff_result diff = ordered_diff<char>::diff(source, target);
+ *    ordered_diff<char>::apply_diff(source, std::move(diff));
+ *    assert(source == target);
+ *
+ * @param T type stored in Container, must provide ==
+ */
+template <typename T, template<typename Y, typename...> typename Container = std::vector>
+requires std::equality_comparable<T> && std::random_access_iterator<typename Container<T>::iterator>
+class ordered_diff {
+public:
+   struct diff_result {
+      Container<size_t>                remove_indexes;
+      Container<std::tuple<size_t, T>> insert_indexes;
+   };
+
+   /// Generate diff_result that when `apply_diff(source, diff_result)` to source will generate target
+   static diff_result diff(const Container<T>& source, const Container<T>& target) {
+      size_t s = 0;
+      size_t t = 0;
+
+      diff_result result;
+      while (s < source.size() || t < target.size()) {
+         if (s < source.size() && t < target.size()) {
+            if (source[s] == target[t]) {
+               // nothing to do, skip over
+               ++s;
+               ++t;
+            } else { // not equal
+               if (s == source.size() - 1 && t == target.size() - 1) {
+                  // both at end, insert target and remove source
+                  result.remove_indexes.push_back(s);
+                  result.insert_indexes.emplace_back(t, target[t]);
+                  ++s;
+                  ++t;
+               } else if (s + 1 < source.size() && t + 1 < target.size() && source[s + 1] == target[t + 1]) {
+                  // misalignment, but next value equal, insert and remove
+                  result.remove_indexes.push_back(s);
+                  result.insert_indexes.emplace_back(t, target[t]);
+                  ++s;
+                  ++t;
+               } else if (t + 1 < target.size() && source[s] == target[t + 1]) {
+                  // source equals next target, insert current target
+                  result.insert_indexes.emplace_back(t, target[t]);
+                  ++t;
+               } else { // source[s + 1] == target[t]
+                  // target matches next source, remove current source
+                  result.remove_indexes.push_back(s);
+                  ++s;
+               }
+            }
+         } else if (s < source.size()) {
+            // remove extra in source
+            result.remove_indexes.push_back(s);
+            ++s;
+         } else if (t < target.size()) {
+            // insert extra in target
+            result.insert_indexes.emplace_back(t, target[t]);
+            ++t;
+         }
+      }
+
+      return result;
+   }
+
+   template <typename X>
+   requires std::same_as<std::decay_t<X>, diff_result>
+   static void apply_diff(Container<T>& source, X&& diff) {
+      // Remove from the source based on diff.remove_indexes
+      std::ptrdiff_t offset = 0;
+      for (size_t index : diff.remove_indexes) {
+         source.erase(source.begin() + index + offset);
+         --offset;
+      }
+
+      // Insert into the source based on diff.insert_indexes
+      for (auto& t : diff.insert_indexes) {
+         size_t index = std::get<0>(t);
+         auto&  value = std::get<1>(t);
+         source.insert(source.begin() + index, std::move(value));
+      }
+   }
+
+}; // class ordered_diff
+
+} // namespace fc
---
+++ libraries/libfc/test/test_ordered_diff.cpp
@@ -0,0 +1,130 @@
+#include <boost/test/unit_test.hpp>
+
+#include <fc/container/ordered_diff.hpp>
+#include <fc/exception/exception.hpp>
+
+using namespace fc;
+
+BOOST_AUTO_TEST_SUITE(ordered_diff_tests)
+
+BOOST_AUTO_TEST_CASE(ordered_diff_test) try {
+   using namespace std;
+
+   { // Basic case
+      vector<char>       source = {'a', 'b', 'c', 'd', 'e'};
+      vector<char>       target = {'a', 'c', 'e', 'f'};
+      auto               result = ordered_diff<char>::diff(source, target);
+      ordered_diff<char>::apply_diff(source, result);
+      BOOST_TEST(source == target);
+   }
+   { // Basic case, deque
+      using ordered_deque_char_diff = ordered_diff<char, std::deque>;
+      deque<char>       source = {'a', 'x', 'c', 'd', 'e'};
+      deque<char>       target = {'z', 'c', 'y', 'f'};
+      auto               result = ordered_deque_char_diff::diff(source, target);
+      ordered_deque_char_diff::apply_diff(source, result);
+      BOOST_TEST(source == target);
+   }
+   { // Empty vectors
+      vector<char> source;
+      vector<char> target;
+      ordered_diff<char>::diff_result result = ordered_diff<char>::diff(source, target);
+      ordered_diff<char>::apply_diff(source, result);
+      BOOST_TEST(source == target);
+   }
+   { // All elements removed
+      vector<char> source = {'a', 'b', 'c', 'd', 'e'};
+      vector<char> target;
+      auto result = ordered_diff<char>::diff(source, target);
+      ordered_diff<char>::apply_diff(source, result);
+      BOOST_TEST(source == target);
+   }
+   { // All elements inserted
+      vector<char> source;
+      vector<char> target = {'a', 'b', 'c', 'd', 'e'};
+      auto result = ordered_diff<char>::diff(source, target);
+      ordered_diff<char>::apply_diff(source, result);
+      BOOST_TEST(source == target);
+   }
+   { // Mix of removals and inserts
+      vector<char> source = {'a', 'b', 'c', 'd', 'e'};
+      vector<char> target = {'a', 'c', 'e', 'f', 'g', 'h'};
+      ordered_diff<char>::diff_result result = ordered_diff<char>::diff(source, target);
+      ordered_diff<char>::apply_diff(source, result);
+      BOOST_TEST(source == target);
+   }
+   { // Mix of removals and inserts
+      vector<int> source = {1, 2, 3, 4, 5};
+      vector<int> target = {3, 4, 6, 2, 0};
+      auto result = ordered_diff<int>::diff(source, target);
+      ordered_diff<int>::apply_diff(source, result);
+      BOOST_TEST(source == target);
+   }
+   { // Complete change
+      vector<char> source = {'a', 'b', 'c', 'd', 'e'};
+      vector<char> target = {'f', 'g', 'h', 'i'};
+      auto result = ordered_diff<char>::diff(source, target);
+      ordered_diff<char>::apply_diff(source, result);
+      BOOST_TEST(source == target);
+   }
+   { // Diff order
+      vector<char> source = {'a', 'b', 'c', 'd', 'e'};
+      vector<char> target = {'e', 'd', 'c', 'b', 'a'};
+      auto result = ordered_diff<char>::diff(source, target);
+      ordered_diff<char>::apply_diff(source, result);
+      BOOST_TEST(source == target);
+   }
+} FC_LOG_AND_RETHROW();
+
+BOOST_AUTO_TEST_CASE(ordered_diff_string_test) try {
+   using namespace std;
+   {
+      vector<string> source = {"hello", "how", "are", "you", "today"};
+      vector<string> target = {"hi", "are", "you", "here"};
+      auto result = ordered_diff<string>::diff(source, target);
+      ordered_diff<string>::apply_diff(source, result);
+      BOOST_TEST(source == target);
+   }
+   {
+      vector<string> source = {"prod1", "prod2", "prod3", "prod4", "prod5"};
+      vector<string> target = {"prod2", "prod1", "prod3", "prod4", "prod5"};;
+      auto result = ordered_diff<string>::diff(source, target);
+      ordered_diff<string>::apply_diff(source, result);
+      BOOST_TEST(source == target);
+   }
+   {
+      vector<string> source = {"prod1", "prod2", "prod3", "prod4", "prod5"};
+      vector<string> target = {"prod5", "prod1", "prod2", "prod3", "prod4"};;
+      auto result = ordered_diff<string>::diff(source, target);
+      ordered_diff<string>::apply_diff(source, std::move(result));
+      BOOST_TEST(source == target);
+   }
+
+} FC_LOG_AND_RETHROW();
+
+class count_moves {
+   std::string s;
+public:
+   inline static size_t num_moves = 0;
+   count_moves(const count_moves& m) : s(m.s) {};
+   count_moves(count_moves&& m) noexcept : s(std::move(m.s)) { ++num_moves; };
+   count_moves& operator=(const count_moves& rhs) = default;
+   explicit count_moves(std::string s) : s(s) {};
+   auto operator<=>(const count_moves&) const = default;
+   bool operator==(const count_moves&) const = default;
+};
+
+BOOST_AUTO_TEST_CASE(ordered_diff_moveable_test) try {
+   using namespace std;
+   {
+      vector<count_moves> source = {count_moves{"hello"}, count_moves{"there"}};
+      vector<count_moves> target = {count_moves{"hi"}, count_moves{"there"}};
+      auto result = ordered_diff<count_moves>::diff(source, target);
+      ordered_diff<count_moves>::apply_diff(source, std::move(result));
+      BOOST_TEST(source == target);
+      BOOST_TEST(count_moves::num_moves == 1);
+   }
+
+} FC_LOG_AND_RETHROW();
+
+BOOST_AUTO_TEST_SUITE_END()
