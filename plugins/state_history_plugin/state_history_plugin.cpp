6c281de8777f31edb2a6e42228702917ae2faa54
Matt Witherspoon <32485495+spoonincode@users.noreply.github.com>
2024-05-06 19:34:52 -0400

    fix thread mixup around connections set

--- plugins/state_history_plugin/state_history_plugin.cpp
+++ plugins/state_history_plugin/state_history_plugin.cpp
@@ -69,6 +69,7 @@ private:
          return std::to_address(lhs) < std::to_address(rhs);
       }
    };
+   boost::asio::strand<boost::asio::io_context::executor_type> connection_set_strand = boost::asio::make_strand(thread_pool.get_executor());
    std::set<std::unique_ptr<session_base>, connection_map_key_less> connections; //gcc 11+ required for unordered_set
 
 public:
@@ -106,7 +107,7 @@ public:
    template <typename Protocol>
    void create_listener(const std::string& address) {
       const boost::posix_time::milliseconds accept_timeout(200);
-      fc::create_listener<Protocol>(thread_pool.get_executor(), _log, accept_timeout, address, "", [this](Protocol::socket&& socket) {
+      fc::create_listener<Protocol>(connection_set_strand, _log, accept_timeout, address, "", [this](Protocol::socket&& socket) {
          catch_and_log([this, &socket]() {
             connections.emplace(new session(std::move(socket), boost::asio::make_strand(thread_pool.get_executor()), chain_plug->chain(),
                                             trace_log, chain_state_log, finality_data_log,
@@ -117,12 +118,11 @@ public:
                                                return get_block(block_num);
                                             },
                                             [this](session_base* conn) {
-                                               boost::asio::post(app().get_io_service(), [conn, this]() { ///TODO: priority?
+                                               boost::asio::post(connection_set_strand, [conn, this]() {
                                                   connections.erase(connections.find(conn));
                                                });
                                             }, _log));
          });
-
       });
    }
 
