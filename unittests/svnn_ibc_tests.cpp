2f9a34e4ae2a69d72c3a22a85ae19eb58a0598dd
Guillaume Babin-Tremblay <gbabint@msn.com>
2024-05-19 10:10:03 -0600

    Restructured Savanna test contracts and added base for finality violation test

--- unittests/svnn_ibc_tests.cpp
+++ unittests/svnn_ibc_tests.cpp
@@ -11,16 +11,12 @@
 
 #include <eosio/chain/exceptions.hpp>
 
-#include "finality_test_cluster.hpp"
+#include "finality_proof.hpp"
 
 using namespace eosio::chain;
 using namespace eosio::testing;
 
 using mvo = mutable_variant_object;
-
-static digest_type hash_pair(const digest_type& a, const digest_type& b) {
-   return fc::sha256::hash(std::pair<const digest_type&, const digest_type&>(a, b));
-}
 
 BOOST_AUTO_TEST_SUITE(svnn_ibc)
 
@@ -41,237 +37,10 @@ BOOST_AUTO_TEST_SUITE(svnn_ibc)
       return {};
    }
 
-   //generate a proof of inclusion for a node at index from a list of leaves
-   std::vector<digest_type> generate_proof_of_inclusion(const std::vector<digest_type> leaves, const size_t index) {
-      auto _leaves = leaves;
-      auto _index = index;
-
-      std::vector<digest_type> merkle_branches;
-
-      while (_leaves.size()>1){
-         std::vector<digest_type> new_level;
-         for (size_t i = 0 ; i < _leaves.size() ; i+=2){
-            digest_type left = _leaves[i];
-
-            if (i + 1 < _leaves.size() && (i + 1 != _leaves.size() - 1 || _leaves.size() % 2 == 0)){
-               // Normal case: both children exist and are not at the end or are even
-               digest_type right = _leaves[i+1];
-
-               new_level.push_back(fc::sha256::hash(std::pair<digest_type, digest_type>(left, right)));
-               if (_index == i || _index == i + 1) {
-                 merkle_branches.push_back(_index == i ? right : left);
-                 _index = i / 2; // Update index for next level
-
-               }
-            } else {
-               // Odd number of leaves at this level, and we're at the end
-               new_level.push_back(left); // Promote the left (which is also the right in this case)
-               if (_index == i) _index = i / 2; // Update index for next level, no sibling to add
-
-            }
-         }
-         _leaves = new_level;
-      }
-      return merkle_branches;
-   }
-
-   bool has_finalizer_policy_diffs(const signed_block_ptr& block){
-
-      // extract new finalizer policy
-      instant_finality_extension if_ext = block->extract_header_extension<instant_finality_extension>();
-
-      if (if_ext.new_finalizer_policy_diff.has_value()) return true;
-      else return false;
-
-   }
-
-   eosio::chain::finalizer_policy update_finalizer_policy(const signed_block_ptr block, eosio::chain::finalizer_policy current_policy){
-
-      // extract new finalizer policy
-      instant_finality_extension if_ext = block->extract_header_extension<instant_finality_extension>();
-
-      assert(if_ext.new_finalizer_policy_diff.has_value());
-
-      eosio::chain::finalizer_policy active_finalizer_policy =
-         current_policy.apply_diff(if_ext.new_finalizer_policy_diff.value());
-
-      return active_finalizer_policy;
-
-   }
-
-   // *****
-
-   // The ibc_cluster class inherits from finality_test_cluster and serves to generate finality proofs in the context of IBC.
-
-   // It has its own high level produce_block function, which hides all the internal finality details, and returns an extended struct containing data relevant to IBC.
-
-   // It also tracks the Savanna state in a rudimentary manner, sufficient to generate the proofs we care about.
-
-   // Since the ibc contract only cares about verifying finality proofs, ibc_cluster doesn't support forks or rollbacks, and always assumes the happy path in finality progression.
-
-   // It also assumes a single producer pre-transition, resulting in only 2 transition blocks.
-
-   // *****
-
-   template<size_t NUM_NODES>
-   class ibc_cluster : public finality_test_cluster<NUM_NODES> {
-   public:
-
-      // data relevant to IBC
-      struct ibc_block_data_t {
-         signed_block_ptr block;
-         action_trace onblock_trace;
-         finality_data_t finality_data;
-         digest_type action_mroot;
-         digest_type base_digest;
-         digest_type active_finalizer_policy_digest;
-         digest_type last_pending_finalizer_policy_digest;
-         digest_type last_proposed_finalizer_policy_digest;
-         digest_type finality_digest;
-         digest_type computed_finality_digest;
-         digest_type afp_base_digest;
-         digest_type finality_leaf;
-         digest_type finality_root;
-      };
-
-      // cache last proposed, last pending and currently active finalizer policies + digests
-      eosio::chain::finalizer_policy last_proposed_finalizer_policy;
-      digest_type last_proposed_finalizer_policy_digest;
-
-      eosio::chain::finalizer_policy last_pending_finalizer_policy;
-      digest_type last_pending_finalizer_policy_digest;
-
-      eosio::chain::finalizer_policy active_finalizer_policy;
-      digest_type active_finalizer_policy_digest;
-
-      // counter to (optimistically) track internal policy changes
-      uint32_t blocks_since_proposed_policy = 0;
-
-      // internal flag to indicate whether or not block is the IF genesis block
-      bool is_genesis = true;
-
-      bool is_transition = true;
-
-      // returns finality leaves for construction of merkle proofs
-      std::vector<digest_type> get_finality_leaves(const size_t cutoff){
-         assert(cutoff>=0 && cutoff<finality_leaves.size());
-         return std::vector<digest_type>(finality_leaves.begin(), finality_leaves.begin() + cutoff + 1);
-      }
-
-      // produce and propagate a block, update internal state as needed, and returns relevant IBC data 
-      ibc_block_data_t produce_block(){
-         auto result = this->produce_and_push_block_ex();
-         signed_block_ptr block = result.block;
-
-         BOOST_REQUIRE(result.onblock_trace->action_traces.size()>0);
-
-         action_trace onblock_trace = result.onblock_trace->action_traces[0];
-
-         // if we have policy diffs, process them
-         if (has_finalizer_policy_diffs(block)){
-
-            if (is_genesis){
-               // if block is genesis, the initial policy is the last proposed, last pending and currently active
-               last_proposed_finalizer_policy = update_finalizer_policy(block, eosio::chain::finalizer_policy());;
-               last_proposed_finalizer_policy_digest = fc::sha256::hash(last_proposed_finalizer_policy);
-               last_pending_finalizer_policy = last_proposed_finalizer_policy;
-               last_pending_finalizer_policy_digest = last_proposed_finalizer_policy_digest;
-               active_finalizer_policy = last_proposed_finalizer_policy;
-               active_finalizer_policy_digest = last_proposed_finalizer_policy_digest;
-            }
-            else {
-               // if block is not genesis, the new policy is proposed
-               last_proposed_finalizer_policy = update_finalizer_policy(block, active_finalizer_policy);
-               last_proposed_finalizer_policy_digest = fc::sha256::hash(last_proposed_finalizer_policy);
-               blocks_since_proposed_policy = 0;
-            }
-
-         }
-
-         // after 3 QCs, the proposed policy becomes pending
-         if (last_pending_finalizer_policy_digest!= last_proposed_finalizer_policy_digest && blocks_since_proposed_policy == 3){
-            last_pending_finalizer_policy = last_proposed_finalizer_policy;
-            last_pending_finalizer_policy_digest = fc::sha256::hash(last_pending_finalizer_policy);
-         }
-
-         // after 3 more QCs (6 total since the policy was proposed) the pending policy becomes active
-         if (active_finalizer_policy_digest!= last_pending_finalizer_policy_digest && blocks_since_proposed_policy == 6){
-            active_finalizer_policy = last_pending_finalizer_policy;
-            active_finalizer_policy_digest = fc::sha256::hash(active_finalizer_policy);
-         }
-
-         blocks_since_proposed_policy++;
-
-         //process votes and collect / compute the IBC-relevant data
-         this->process_votes(1, this->num_needed_for_quorum); //enough to reach quorum threshold
-         finality_data_t finality_data = *this->node0.control->head_finality_data();
-         digest_type action_mroot = finality_data.action_mroot;
-         digest_type base_digest = finality_data.base_digest;
-         digest_type afp_base_digest = hash_pair(last_pending_finalizer_policy_digest, base_digest);
-         digest_type finality_digest;
-
-         if (is_genesis){
-
-            // one-time genesis finality digest computation
-            finality_digest = fc::sha256::hash(eosio::chain::finality_digest_data_v1{
-               .active_finalizer_policy_generation      = 1,
-               .finality_tree_digest                    = digest_type(), //nothing to finalize yet
-               .last_pending_finalizer_policy_and_base_digest = afp_base_digest
-            });
-         }
-         else finality_digest = this->node0.control->get_strong_digest_by_id(block->calculate_id());
-
-         // compute finality leaf
-         digest_type finality_leaf = fc::sha256::hash(valid_t::finality_leaf_node_t{
-            .block_num = block->block_num(),
-            .finality_digest = finality_digest,
-            .action_mroot = action_mroot
-         });
-
-         // during IF transition, finality_root is always set to an empty digest
-         digest_type finality_root = digest_type();
-
-         // after transition, finality_root can be obtained from the action_mroot field of the block header
-         if (!is_transition) finality_root = block->action_mroot;
-
-         // compute digest for verification purposes
-         digest_type computed_finality_digest = fc::sha256::hash(eosio::chain::finality_digest_data_v1{
-               .active_finalizer_policy_generation      = active_finalizer_policy.generation,
-               .finality_tree_digest                    = is_genesis ? digest_type() : finality_root,
-               .last_pending_finalizer_policy_and_base_digest = afp_base_digest
-            });
-
-         // add finality leaf to the internal list
-         finality_leaves.push_back(finality_leaf);
-
-         if (is_transition && !is_genesis) is_transition = false; // if we are no longer in transition mode, set to false
-         if (is_genesis) is_genesis = false; // if IF genesis block, set to false 
-
-         // return relevant IBC information
-         return {block, onblock_trace, finality_data, action_mroot, base_digest, active_finalizer_policy_digest, last_pending_finalizer_policy_digest, last_proposed_finalizer_policy_digest, finality_digest, computed_finality_digest, afp_base_digest, finality_leaf, finality_root };
-
-      }
-
-      void produce_blocks(const uint32_t count){
-         for (uint32_t i = 0 ; i < count ; i++){
-            produce_block();
-         } 
-      }
-
-      ibc_cluster(finality_cluster_config_t config = {.transition_to_savanna = false})
-      : finality_test_cluster<NUM_NODES>(config) {
-
-      }
-
-   private:
-      std::vector<digest_type> finality_leaves;
-
-   };
-
    BOOST_AUTO_TEST_CASE(ibc_test) { try {
 
       // cluster is set up with the head about to produce IF Genesis
-      ibc_cluster<4> cluster;
+      finality_proof::proof_test_cluster<4> cluster;
 
       // produce IF Genesis block
       auto genesis_block_result = cluster.produce_block();
@@ -319,11 +88,13 @@ BOOST_AUTO_TEST_SUITE(svnn_ibc)
       BOOST_TEST(qc_b_6.qc.has_value());
 
       std::stringstream sstream;
-      sstream << std::hex << (1 << (cluster.num_needed_for_quorum + 1)) - 1; // we expect a quorum of finalizers to vote
+      sstream << std::hex << (1 << 3) - 1; // we expect a quorum of finalizers to vote
                                                                              // +1 because num_needed_for_quorum excludes node0
       std::string raw_bitset = sstream.str();
       if (raw_bitset.size() % 2)
          raw_bitset.insert(0, "0");
+
+      std::cout << "raw_bitset : " << raw_bitset << "\n";
 
       // create a few proofs we'll use to perform tests
 
@@ -361,7 +132,7 @@ BOOST_AUTO_TEST_SUITE(svnn_ibc)
                      ("action_mroot", block_2_result.action_mroot)
                   )
                )
-               ("merkle_branches", generate_proof_of_inclusion(cluster.get_finality_leaves(2), 2))
+               ("merkle_branches", finality_proof::generate_proof_of_inclusion(cluster.get_finality_leaves(2), 2))
             )
          );
 
@@ -399,7 +170,7 @@ BOOST_AUTO_TEST_SUITE(svnn_ibc)
                      ("action_mroot", block_2_result.action_mroot)
                   )
                )
-               ("merkle_branches", generate_proof_of_inclusion(cluster.get_finality_leaves(3), 2))
+               ("merkle_branches", finality_proof::generate_proof_of_inclusion(cluster.get_finality_leaves(3), 2))
             )
          );
 
@@ -424,7 +195,7 @@ BOOST_AUTO_TEST_SUITE(svnn_ibc)
                      ("action_mroot", block_2_result.action_mroot)
                   )
                )
-               ("merkle_branches", generate_proof_of_inclusion(cluster.get_finality_leaves(2), 2))
+               ("merkle_branches", finality_proof::generate_proof_of_inclusion(cluster.get_finality_leaves(2), 2))
             )
          );
       
@@ -452,10 +223,10 @@ BOOST_AUTO_TEST_SUITE(svnn_ibc)
       action_leaves.push_back(check_heavy_proof_2_trace.digest_savanna());
 
       // since a few actions were included in the previous block, we can verify that they correctly hash into the action_mroot for that block
-      auto pair_1_hash = hash_pair(action_leaves[0], action_leaves[1]);
-      auto pair_2_hash = hash_pair(action_leaves[2], action_leaves[3]);
+      auto pair_1_hash = finality_proof::hash_pair(action_leaves[0], action_leaves[1]);
+      auto pair_2_hash = finality_proof::hash_pair(action_leaves[2], action_leaves[3]);
 
-      auto computed_action_mroot = hash_pair(pair_1_hash, pair_2_hash);
+      auto computed_action_mroot = finality_proof::hash_pair(pair_1_hash, pair_2_hash);
 
       BOOST_CHECK(computed_action_mroot == block_7_result.action_mroot);
 
@@ -473,7 +244,7 @@ BOOST_AUTO_TEST_SUITE(svnn_ibc)
       auto block_8_result = cluster.produce_block();
 
       // verify the block header contains the proposed finalizer policy differences
-      BOOST_TEST(has_finalizer_policy_diffs(block_8_result.block));
+      BOOST_TEST(finality_proof::has_finalizer_policy_diffs(block_8_result.block));
 
       // advance finality
       auto block_9_result = cluster.produce_block();
@@ -508,7 +279,7 @@ BOOST_AUTO_TEST_SUITE(svnn_ibc)
             ("recv_sequence", block_7_result.onblock_trace.receipt.value().recv_sequence)
             ("witness_hash", block_7_result.onblock_trace.savanna_witness_hash())
          )
-         ("merkle_branches", generate_proof_of_inclusion(action_leaves, 0));
+         ("merkle_branches", finality_proof::generate_proof_of_inclusion(action_leaves, 0));
 
 
       // first action proof (check_heavy_proof_1)
@@ -527,7 +298,7 @@ BOOST_AUTO_TEST_SUITE(svnn_ibc)
             ("recv_sequence", check_heavy_proof_1_trace.receipt.value().recv_sequence)
             ("witness_hash", check_heavy_proof_1_trace.savanna_witness_hash())
          )
-         ("merkle_branches", generate_proof_of_inclusion(action_leaves, 1));
+         ("merkle_branches", finality_proof::generate_proof_of_inclusion(action_leaves, 1));
 
       // second action proof (check_light_proof_1)
       mutable_variant_object action_proof_2 = mvo()
@@ -545,7 +316,7 @@ BOOST_AUTO_TEST_SUITE(svnn_ibc)
             ("recv_sequence", check_light_proof_1_trace.receipt.value().recv_sequence)
             ("witness_hash", check_light_proof_1_trace.savanna_witness_hash())
          )
-         ("merkle_branches", generate_proof_of_inclusion(action_leaves, 2));
+         ("merkle_branches", finality_proof::generate_proof_of_inclusion(action_leaves, 2));
 
       // proof to verify the inclusion of onblock action via heavy proof
       mutable_variant_object action_heavy_proof = mvo()
@@ -580,7 +351,7 @@ BOOST_AUTO_TEST_SUITE(svnn_ibc)
                      ("action_proofs", fc::variants({onblock_action_proof}))
                   )
                )
-               ("merkle_branches", generate_proof_of_inclusion(cluster.get_finality_leaves(7), 7))
+               ("merkle_branches", finality_proof::generate_proof_of_inclusion(cluster.get_finality_leaves(7), 7))
             )
          );
 
@@ -604,7 +375,7 @@ BOOST_AUTO_TEST_SUITE(svnn_ibc)
                      ("action_proofs", fc::variants({action_proof_1, action_proof_2}))
                   )
                )
-               ("merkle_branches", generate_proof_of_inclusion(cluster.get_finality_leaves(7), 7))
+               ("merkle_branches", finality_proof::generate_proof_of_inclusion(cluster.get_finality_leaves(7), 7))
             )
          );
 
@@ -702,7 +473,7 @@ BOOST_AUTO_TEST_SUITE(svnn_ibc)
                      ("action_mroot", block_11_result.action_mroot)
                   )
                )
-               ("merkle_branches", generate_proof_of_inclusion(cluster.get_finality_leaves(11), 11))
+               ("merkle_branches", finality_proof::generate_proof_of_inclusion(cluster.get_finality_leaves(11), 11))
             )
          );
 
@@ -747,7 +518,7 @@ BOOST_AUTO_TEST_SUITE(svnn_ibc)
                      ("action_mroot", block_12_result.action_mroot)
                   )
                )
-               ("merkle_branches", generate_proof_of_inclusion(cluster.get_finality_leaves(12), 12))
+               ("merkle_branches", finality_proof::generate_proof_of_inclusion(cluster.get_finality_leaves(12), 12))
             )
          );
 
