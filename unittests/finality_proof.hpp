bf90b9a907289ce86470a73f42315ad9a2159aae
Guillaume Babin-Tremblay <gbabint@msn.com>
2024-05-19 10:58:08 -0600

    Completed initial finality violation test, added finality violation test, common test headers called finality_proof.hpp

---
+++ unittests/finality_proof.hpp
@@ -0,0 +1,248 @@
+#pragma once
+
+#include "finality_test_cluster.hpp"
+
+/*****
+
+   The proof_test_cluster class inherits from finality_test_cluster and serves to generate finality proofs in the context of IBC and finality violation.
+
+   It has its own high-level produce_block function, which hides all the internal finality details, and returns an extended struct containing data relevant for proof generation.
+
+   It doesn't support forks or rollbacks, and always assumes the happy path in finality progression, which is sufficient for the purpose of generating finality proofs.
+   
+   It also assumes a single producer pre-transition, resulting in only 2 transition blocks.
+
+*****/
+
+namespace finality_proof {
+
+   //generate a proof of inclusion for a node at index from a list of leaves
+   static std::vector<digest_type> generate_proof_of_inclusion(const std::vector<digest_type> leaves, const size_t index) {
+      auto _leaves = leaves;
+      auto _index = index;
+
+      std::vector<digest_type> merkle_branches;
+
+      while (_leaves.size()>1){
+         std::vector<digest_type> new_level;
+         for (size_t i = 0 ; i < _leaves.size() ; i+=2){
+            digest_type left = _leaves[i];
+
+            if (i + 1 < _leaves.size() && (i + 1 != _leaves.size() - 1 || _leaves.size() % 2 == 0)){
+               // Normal case: both children exist and are not at the end or are even
+               digest_type right = _leaves[i+1];
+
+               new_level.push_back(fc::sha256::hash(std::pair<digest_type, digest_type>(left, right)));
+               if (_index == i || _index == i + 1) {
+                 merkle_branches.push_back(_index == i ? right : left);
+                 _index = i / 2; // Update index for next level
+
+               }
+            } else {
+               // Odd number of leaves at this level, and we're at the end
+               new_level.push_back(left); // Promote the left (which is also the right in this case)
+               if (_index == i) _index = i / 2; // Update index for next level, no sibling to add
+
+            }
+         }
+         _leaves = new_level;
+      }
+      return merkle_branches;
+   }
+
+   static digest_type hash_pair(const digest_type& a, const digest_type& b) {
+      return fc::sha256::hash(std::pair<const digest_type&, const digest_type&>(a, b));
+   }
+
+   static bool has_finalizer_policy_diffs(const signed_block_ptr& block){
+
+      // extract new finalizer policy
+      instant_finality_extension if_ext = block->extract_header_extension<instant_finality_extension>();
+
+      if (if_ext.new_finalizer_policy_diff.has_value()) return true;
+      else return false;
+
+   }
+
+   static eosio::chain::finalizer_policy update_finalizer_policy(const signed_block_ptr block, eosio::chain::finalizer_policy current_policy){
+
+      // extract new finalizer policy
+      instant_finality_extension if_ext = block->extract_header_extension<instant_finality_extension>();
+
+      assert(if_ext.new_finalizer_policy_diff.has_value());
+
+      eosio::chain::finalizer_policy active_finalizer_policy =
+         current_policy.apply_diff(if_ext.new_finalizer_policy_diff.value());
+
+      return active_finalizer_policy;
+
+   }
+
+   template<size_t NUM_NODES>
+   class proof_test_cluster : public finality_test_cluster<NUM_NODES> {
+   public:
+
+      // data relevant to IBC
+      struct ibc_block_data_t {
+         signed_block_ptr block;
+         action_trace onblock_trace;
+         finality_data_t finality_data;
+         digest_type action_mroot;
+         digest_type base_digest;
+         digest_type active_finalizer_policy_digest;
+         digest_type last_pending_finalizer_policy_digest;
+         digest_type last_proposed_finalizer_policy_digest;
+         digest_type finality_digest;
+         digest_type computed_finality_digest;
+         digest_type afp_base_digest;
+         digest_type finality_leaf;
+         digest_type finality_root;
+      };
+
+      // cache last proposed, last pending and currently active finalizer policies + digests
+      eosio::chain::finalizer_policy last_proposed_finalizer_policy;
+      digest_type last_proposed_finalizer_policy_digest;
+
+      eosio::chain::finalizer_policy last_pending_finalizer_policy;
+      digest_type last_pending_finalizer_policy_digest;
+
+      eosio::chain::finalizer_policy active_finalizer_policy;
+      digest_type active_finalizer_policy_digest;
+
+      // counter to (optimistically) track internal policy changes
+      uint32_t blocks_since_proposed_policy = 0;
+
+      //node0 always produce blocks. This vector determines if votes from node1, node2, etc. are propagated
+      std::vector<bool> vote_propagation;
+      
+      // internal flag to indicate whether or not block is the IF genesis block
+      bool is_genesis = true;
+
+      bool is_transition = true;
+
+      // returns finality leaves for construction of merkle proofs
+      std::vector<digest_type> get_finality_leaves(const size_t cutoff){
+         assert(cutoff>=0 && cutoff<finality_leaves.size());
+         return std::vector<digest_type>(finality_leaves.begin(), finality_leaves.begin() + cutoff + 1);
+      }
+
+      ibc_block_data_t process_result(eosio::testing::produce_block_result_t result){
+
+         signed_block_ptr block = result.block;
+
+         BOOST_REQUIRE(result.onblock_trace->action_traces.size()>0);
+
+         action_trace onblock_trace = result.onblock_trace->action_traces[0];
+
+         // if we have policy diffs, process them
+         if (has_finalizer_policy_diffs(block)){
+
+            if (is_genesis){
+               // if block is genesis, the initial policy is the last proposed, last pending and currently active
+               last_proposed_finalizer_policy = update_finalizer_policy(block, eosio::chain::finalizer_policy());;
+               last_proposed_finalizer_policy_digest = fc::sha256::hash(last_proposed_finalizer_policy);
+               last_pending_finalizer_policy = last_proposed_finalizer_policy;
+               last_pending_finalizer_policy_digest = last_proposed_finalizer_policy_digest;
+               active_finalizer_policy = last_proposed_finalizer_policy;
+               active_finalizer_policy_digest = last_proposed_finalizer_policy_digest;
+            }
+            else {
+               // if block is not genesis, the new policy is proposed
+               last_proposed_finalizer_policy = update_finalizer_policy(block, active_finalizer_policy);
+               last_proposed_finalizer_policy_digest = fc::sha256::hash(last_proposed_finalizer_policy);
+               blocks_since_proposed_policy = 0;
+            }
+
+         }
+
+         // after 3 QCs, the proposed policy becomes pending
+         if (last_pending_finalizer_policy_digest!= last_proposed_finalizer_policy_digest && blocks_since_proposed_policy == 3){
+            last_pending_finalizer_policy = last_proposed_finalizer_policy;
+            last_pending_finalizer_policy_digest = fc::sha256::hash(last_pending_finalizer_policy);
+         }
+
+         // after 3 more QCs (6 total since the policy was proposed) the pending policy becomes active
+         if (active_finalizer_policy_digest!= last_pending_finalizer_policy_digest && blocks_since_proposed_policy == 6){
+            active_finalizer_policy = last_pending_finalizer_policy;
+            active_finalizer_policy_digest = fc::sha256::hash(active_finalizer_policy);
+         }
+
+         blocks_since_proposed_policy++;
+
+         //process votes and collect / compute the IBC-relevant data
+
+         if (vote_propagation.size() == 0) this->process_votes(1, this->num_needed_for_quorum); //enough to reach quorum threshold
+         else this->process_finalizer_votes(vote_propagation); //enough to reach quorum threshold
+
+         finality_data_t finality_data = *this->node0.control->head_finality_data();
+         digest_type action_mroot = finality_data.action_mroot;
+         digest_type base_digest = finality_data.base_digest;
+         digest_type afp_base_digest = hash_pair(last_pending_finalizer_policy_digest, base_digest);
+         digest_type finality_digest;
+
+         if (is_genesis){
+
+            // one-time genesis finality digest computation
+            finality_digest = fc::sha256::hash(eosio::chain::finality_digest_data_v1{
+               .active_finalizer_policy_generation      = 1,
+               .finality_tree_digest                    = digest_type(), //nothing to finalize yet
+               .last_pending_finalizer_policy_and_base_digest = afp_base_digest
+            });
+         }
+         else finality_digest = this->node0.control->get_strong_digest_by_id(block->calculate_id());
+
+         // compute finality leaf
+         digest_type finality_leaf = fc::sha256::hash(valid_t::finality_leaf_node_t{
+            .block_num = block->block_num(),
+            .finality_digest = finality_digest,
+            .action_mroot = action_mroot
+         });
+
+         // during IF transition, finality_root is always set to an empty digest
+         digest_type finality_root = digest_type();
+
+         // after transition, finality_root can be obtained from the action_mroot field of the block header
+         if (!is_transition) finality_root = block->action_mroot;
+
+         // compute digest for verification purposes
+         digest_type computed_finality_digest = fc::sha256::hash(eosio::chain::finality_digest_data_v1{
+               .active_finalizer_policy_generation      = active_finalizer_policy.generation,
+               .finality_tree_digest                    = is_genesis ? digest_type() : finality_root,
+               .last_pending_finalizer_policy_and_base_digest = afp_base_digest
+            });
+
+         // add finality leaf to the internal list
+         finality_leaves.push_back(finality_leaf);
+
+         if (is_transition && !is_genesis) is_transition = false; // if we are no longer in transition mode, set to false
+         if (is_genesis) is_genesis = false; // if IF genesis block, set to false 
+
+         // return relevant IBC information
+         return {block, onblock_trace, finality_data, action_mroot, base_digest, active_finalizer_policy_digest, last_pending_finalizer_policy_digest, last_proposed_finalizer_policy_digest, finality_digest, computed_finality_digest, afp_base_digest, finality_leaf, finality_root };
+
+      }
+
+      // produce and propagate a block, update internal state as needed, and returns relevant IBC data 
+      ibc_block_data_t produce_block(){
+         assert(vote_propagation.size() == 0 || vote_propagation.size() == NUM_NODES-1);
+         eosio::testing::produce_block_result_t result = this->produce_and_push_block_ex();
+         return(process_result(result));
+      }
+
+      void produce_blocks(const uint32_t count){
+         for (uint32_t i = 0 ; i < count ; i++){
+            produce_block();
+         } 
+      }
+
+      proof_test_cluster(finality_cluster_config_t config = {.transition_to_savanna = false})
+      : finality_test_cluster<NUM_NODES>(config) {
+
+      }
+
+   private:
+      std::vector<digest_type> finality_leaves;
+
+   };
+
+}
---
+++ unittests/svnn_finality_violation_tests.cpp
@@ -0,0 +1,304 @@
+#include <eosio/chain/abi_serializer.hpp>
+#include <eosio/testing/tester.hpp>
+
+#include <fc/variant_object.hpp>
+
+#include <boost/test/unit_test.hpp>
+
+#include <contracts.hpp>
+#include <test_contracts.hpp>
+#include "fork_test_utilities.hpp"
+
+#include <eosio/chain/exceptions.hpp>
+
+#include "finality_proof.hpp"
+
+using namespace eosio::chain;
+using namespace eosio::testing;
+
+using mvo = mutable_variant_object;
+
+BOOST_AUTO_TEST_SUITE(svnn_finality_violation)
+
+   //extract instant finality data from block header extension, as well as qc data from block extension
+   qc_data_t extract_qc_data(const signed_block_ptr& b) {
+      auto hexts = b->validate_and_extract_header_extensions();
+      if (auto if_entry = hexts.lower_bound(instant_finality_extension::extension_id()); if_entry != hexts.end()) {
+         auto& if_ext   = std::get<instant_finality_extension>(if_entry->second);
+
+         // get the matching qc extension if present
+         auto exts = b->validate_and_extract_extensions();
+         if (auto entry = exts.lower_bound(quorum_certificate_extension::extension_id()); entry != exts.end()) {
+            auto& qc_ext = std::get<quorum_certificate_extension>(entry->second);
+            return qc_data_t{ std::move(qc_ext.qc), if_ext.qc_claim };
+         }
+         return qc_data_t{ {}, if_ext.qc_claim };
+      }
+      return {};
+   }
+
+   BOOST_AUTO_TEST_CASE(finality_violation_test) { try {
+
+      /* 
+
+         In this test, node0 and node1 are colluding to create a finality violation.
+       
+         node0 ande node1 partition the network in such a way that node2 and node3, who are both honest nodes, are disconnected from each other.
+
+         node2 is tricked into joining a fake chain where node0 and node1 are finalizing, but from which node3 is absent.
+
+         In the meanwhile, node2 is absent from the real chain, but node0 and node1 are still finalizing on it, therefore double-signing between the fake and real chains.
+
+         node3, unaware of the ongoing attack, is finalizing on the real chain normally.
+
+         node2 is able to rejoin the real chain later, and to prove the finality violation.
+
+      */
+
+      // setup a fake chain
+      finality_proof::proof_test_cluster<4> fake_chain;
+      finality_proof::proof_test_cluster<4> real_chain;
+
+      fake_chain.node0.create_accounts( { "user1"_n, "user2"_n, "violation"_n, "eosio.token"_n } );
+
+      fake_chain.node0.set_code( "eosio.token"_n, test_contracts::eosio_token_wasm() );
+      fake_chain.node0.set_abi( "eosio.token"_n, test_contracts::eosio_token_abi() );
+
+      fake_chain.node0.set_code( "violation"_n, test_contracts::finality_violation_wasm() );
+      fake_chain.node0.set_abi( "violation"_n, test_contracts::finality_violation_abi() );
+
+      //node3 doesn't receive votes on the fake chain
+      fake_chain.vote_propagation = {1, 1, 0};
+
+      //  and setup a real chain
+      real_chain.node0.create_accounts( { "user1"_n, "user2"_n, "violation"_n, "eosio.token"_n } );
+
+      real_chain.node0.set_code( "eosio.token"_n, test_contracts::eosio_token_wasm() );
+      real_chain.node0.set_abi( "eosio.token"_n, test_contracts::eosio_token_abi() );
+
+      real_chain.node0.set_code( "violation"_n, test_contracts::finality_violation_wasm() );
+      real_chain.node0.set_abi( "violation"_n, test_contracts::finality_violation_abi() );
+
+      //node2 doesn't receive votes on the real chain
+      real_chain.vote_propagation = {1, 0, 1};
+
+      mutable_variant_object create_action = mvo()
+         ( "issuer", "eosio"_n)
+         ( "maximum_supply", "100.0000 EOS");
+
+      mutable_variant_object issue_action = mvo()
+         ( "to", "eosio"_n)
+         ( "quantity", "100.0000 EOS")
+         ( "memo", "");
+
+      mutable_variant_object initial_transfer = mvo()
+         ("from", "eosio"_n)
+         ("to", "user1"_n)
+         ("quantity", "1.0000 EOS")
+         ("memo", "");
+
+      fake_chain.node0.push_action("eosio.token"_n, "create"_n, "eosio.token"_n, create_action);
+      fake_chain.node0.push_action("eosio.token"_n, "issue"_n, "eosio"_n, issue_action);
+      fake_chain.node0.push_action("eosio.token"_n, "transfer"_n, "eosio"_n, initial_transfer);
+
+      real_chain.node0.push_action("eosio.token"_n, "create"_n, "eosio.token"_n, create_action);
+      real_chain.node0.push_action("eosio.token"_n, "issue"_n, "eosio"_n, issue_action);
+      real_chain.node0.push_action("eosio.token"_n, "transfer"_n, "eosio"_n, initial_transfer);
+
+      //we produce the IF genesis block on both chains
+      auto fake_genesis_block_result = fake_chain.produce_block();
+      auto real_genesis_block_result = real_chain.produce_block();
+
+      //we verify that we have the same finalizer policy on both chains
+      BOOST_TEST(fake_chain.active_finalizer_policy_digest == real_chain.active_finalizer_policy_digest);
+
+      //we produce enough block to complete the IF transition, and a few more
+      auto fake_block_1_result = fake_chain.produce_block();
+      auto real_block_1_result = real_chain.produce_block();
+
+      auto fake_block_2_result = fake_chain.produce_block();
+      auto real_block_2_result = real_chain.produce_block();
+
+      auto fake_block_3_result = fake_chain.produce_block();
+      auto real_block_3_result = real_chain.produce_block();
+
+      auto fake_block_4_result = fake_chain.produce_block();
+      auto real_block_4_result = real_chain.produce_block();
+
+      auto fake_block_5_result = fake_chain.produce_block();
+      auto real_block_5_result = real_chain.produce_block();
+
+      auto fake_block_6_result = fake_chain.produce_block();
+      auto real_block_6_result = real_chain.produce_block();
+
+      //verify that the two chains are the same so far
+      BOOST_TEST(fake_genesis_block_result.finality_leaf==real_genesis_block_result.finality_leaf);
+      BOOST_TEST(fake_block_1_result.finality_leaf==real_block_1_result.finality_leaf);
+      BOOST_TEST(fake_block_2_result.finality_leaf==real_block_2_result.finality_leaf);
+      BOOST_TEST(fake_block_3_result.finality_leaf==real_block_3_result.finality_leaf);
+      BOOST_TEST(fake_block_4_result.finality_leaf==real_block_4_result.finality_leaf);
+      BOOST_TEST(fake_block_5_result.finality_leaf==real_block_5_result.finality_leaf);
+      BOOST_TEST(fake_block_6_result.finality_leaf==real_block_6_result.finality_leaf);
+
+      qc_data_t fake_qc_b_4 = extract_qc_data(fake_block_4_result.block);
+      qc_data_t real_qc_b_4 = extract_qc_data(real_block_4_result.block);
+
+      qc_data_t fake_qc_b_5 = extract_qc_data(fake_block_5_result.block);
+      qc_data_t real_qc_b_5 = extract_qc_data(real_block_5_result.block);
+
+      qc_data_t fake_qc_b_6 = extract_qc_data(fake_block_6_result.block);
+      qc_data_t real_qc_b_6 = extract_qc_data(real_block_6_result.block);
+
+      //verify that we have QCs on block #4 and onward for both chains
+      BOOST_TEST(fake_qc_b_4.qc.has_value());
+      BOOST_TEST(real_qc_b_4.qc.has_value());
+      BOOST_TEST(fake_qc_b_5.qc.has_value());
+      BOOST_TEST(real_qc_b_5.qc.has_value());
+      BOOST_TEST(fake_qc_b_6.qc.has_value());
+      BOOST_TEST(real_qc_b_6.qc.has_value());
+
+      //verify that the signatures on QCs are different on both chains
+      BOOST_TEST(fake_qc_b_4.qc.value().data.sig.to_string() != real_qc_b_4.qc.value().data.sig.to_string());
+      BOOST_TEST(fake_qc_b_5.qc.value().data.sig.to_string() != real_qc_b_5.qc.value().data.sig.to_string());
+      BOOST_TEST(fake_qc_b_6.qc.value().data.sig.to_string() != real_qc_b_6.qc.value().data.sig.to_string());
+
+      //now, we create and push a transaction to the fake chain
+      mutable_variant_object transfer = mvo()
+         ("from", "user1"_n)
+         ("to", "user2"_n)
+         ("quantity", "1.0000 EOS")
+         ("memo", "");
+
+      fake_chain.node0.push_action("eosio.token"_n, "transfer"_n, "user1"_n, transfer);
+
+      // the double-signing of block_7 is the "original sin", where the fake and the real chain start diverging
+      auto fake_block_7_result = fake_chain.produce_block();
+      auto real_block_7_result = real_chain.produce_block();
+
+      //we verify that fake and real finality leaves for block_7 are different, which result in two different chains that are both valid 
+      BOOST_TEST(fake_block_7_result.finality_leaf!=real_block_7_result.finality_leaf);
+
+      auto fake_block_8_result = fake_chain.produce_block();
+      auto real_block_8_result = real_chain.produce_block();
+
+      auto fake_block_9_result = fake_chain.produce_block();
+      auto real_block_9_result = real_chain.produce_block();   
+
+      //the QC contained in block_10 is over block_9, which makes a different version of block_7 final on the fake and on the real chains
+      auto fake_block_10_result = fake_chain.produce_block();
+      auto real_block_10_result = real_chain.produce_block();
+
+      //we verify that we still have the same finalizer policy on both chains
+      BOOST_TEST(fake_chain.active_finalizer_policy_digest == real_chain.active_finalizer_policy_digest);
+
+      qc_data_t fake_qc_b_7 = extract_qc_data(fake_block_7_result.block);
+      qc_data_t real_qc_b_7 = extract_qc_data(real_block_7_result.block);
+
+      qc_data_t fake_qc_b_8 = extract_qc_data(fake_block_8_result.block);
+      qc_data_t real_qc_b_8 = extract_qc_data(real_block_8_result.block);
+
+      qc_data_t fake_qc_b_9 = extract_qc_data(fake_block_9_result.block);
+      qc_data_t real_qc_b_9 = extract_qc_data(real_block_9_result.block);
+
+      qc_data_t fake_qc_b_10 = extract_qc_data(fake_block_10_result.block);
+      qc_data_t real_qc_b_10 = extract_qc_data(real_block_10_result.block);
+
+      BOOST_TEST(fake_qc_b_7.qc.has_value());
+      BOOST_TEST(real_qc_b_7.qc.has_value());
+      BOOST_TEST(fake_qc_b_8.qc.has_value());
+      BOOST_TEST(real_qc_b_8.qc.has_value());
+      BOOST_TEST(fake_qc_b_9.qc.has_value());
+      BOOST_TEST(real_qc_b_9.qc.has_value());
+      BOOST_TEST(fake_qc_b_10.qc.has_value());
+      BOOST_TEST(real_qc_b_10.qc.has_value());
+
+      //bitsets for the fake and real chains
+      std::string fake_bitset("07");
+      std::string real_bitset("0b");
+
+      //assemble finality violation proof
+      mutable_variant_object finalizer_policy = mvo()
+         ("from_block_num", 1)
+         ("policy", real_chain.active_finalizer_policy);
+
+      mutable_variant_object proof1 = mvo()
+            ("finality_proof", mvo() //proves finality of block #2
+               ("qc_block", mvo()
+                  ("major_version", 1)
+                  ("minor_version", 0)
+                  ("finalizer_policy_generation", 1)
+                  ("witness_hash", fake_block_9_result.afp_base_digest)
+                  ("finality_mroot", fake_block_9_result.finality_root)
+               )
+               ("qc", mvo()
+                  ("signature", fake_qc_b_10.qc.value().data.sig.to_string())
+                  ("finalizers", fake_bitset) 
+               )
+            )
+            ("target_block_proof_of_inclusion", mvo() 
+               ("target_node_index", 7)
+               ("last_node_index", 7)
+               ("target",  mvo() //target block #2
+                  ("finality_data", mvo() 
+                     ("major_version", 1)
+                     ("minor_version", 0)
+                     ("finalizer_policy_generation", 1)
+                     ("witness_hash", fake_block_7_result.afp_base_digest)
+                     ("finality_mroot", fake_block_7_result.finality_root)
+                  )
+                  ("dynamic_data", mvo() 
+                     ("block_num", fake_block_7_result.block->block_num())
+                     ("action_proofs", fc::variants())
+                     ("action_mroot", fake_block_7_result.action_mroot)
+                  )
+               )
+               ("merkle_branches", finality_proof::generate_proof_of_inclusion(fake_chain.get_finality_leaves(7), 7))
+            );
+
+      mutable_variant_object proof2 = mvo()
+            ("finality_proof", mvo() //proves finality of block #2
+               ("qc_block", mvo()
+                  ("major_version", 1)
+                  ("minor_version", 0)
+                  ("finalizer_policy_generation", 1)
+                  ("witness_hash", real_block_9_result.afp_base_digest)
+                  ("finality_mroot", real_block_9_result.finality_root)
+               )
+               ("qc", mvo()
+                  ("signature", real_qc_b_10.qc.value().data.sig.to_string())
+                  ("finalizers", real_bitset) 
+               )
+            )
+            ("target_block_proof_of_inclusion", mvo() 
+               ("target_node_index", 7)
+               ("last_node_index", 7)
+               ("target",  mvo() //target block #2
+                  ("finality_data", mvo() 
+                     ("major_version", 1)
+                     ("minor_version", 0)
+                     ("finalizer_policy_generation", 1)
+                     ("witness_hash", real_block_7_result.afp_base_digest)
+                     ("finality_mroot", real_block_7_result.finality_root)
+                  )
+                  ("dynamic_data", mvo() 
+                     ("block_num", real_block_7_result.block->block_num())
+                     ("action_proofs", fc::variants())
+                     ("action_mroot", real_block_7_result.action_mroot)
+                  )
+               )
+               ("merkle_branches", finality_proof::generate_proof_of_inclusion(real_chain.get_finality_leaves(7), 7))
+            );
+      
+      mutable_variant_object finality_violation_proof = mvo()
+         ("finalizer_policy", real_chain.active_finalizer_policy)
+         ("proof1", proof1)
+         ("proof2", proof2);
+
+      //submit finality violation proof to the smart contract
+      real_chain.node0.push_action("violation"_n, "addviolation"_n, "user1"_n, finality_violation_proof);
+
+      //if the proof was accepted, it means the finality violation has been proven
+
+   } FC_LOG_AND_RETHROW() }
+
+BOOST_AUTO_TEST_SUITE_END()
