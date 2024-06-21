b307f4c920d4dad318be0dc1bbc6ad78278f7df0
Lin Huang <lin.huang@eosnetwork.com>
2024-06-20 21:43:08 -0400

    Restructure tests and add more comments so that they are easier to understand

--- unittests/block_header_state_tests.cpp
+++ unittests/block_header_state_tests.cpp
@@ -22,12 +22,13 @@ block_id_type id_from_num(block_num_type block_number) {
 BOOST_AUTO_TEST_CASE(future_proposed_pending_test) try {
    // When the block associated with a policy is not final (block_num is greater than LIB),
    // keep the policy as is.
-   block_header_state prev;
-   prev.proposed_finalizer_policies.emplace_back(std::make_pair(5u, nullptr)); // block_num 5
-   prev.pending_finalizer_policy = std::make_pair(4u, nullptr); // block_num 4
 
+   block_header_state prev;
    block_header_state next_header_state;
+
    next_header_state.core.links.emplace_back(qc_link{.target_block_num = 1u}); // lib
+   prev.proposed_finalizer_policies.emplace_back(std::make_pair(5u, nullptr)); // proposed_finalizer_policy block_num 5
+   prev.pending_finalizer_policy = std::make_pair(4u, nullptr); // pending_finalizer_policy block_num 4
 
    evaluate_finalizer_policies_for_promotion(prev, next_header_state);
 
@@ -36,14 +37,15 @@ BOOST_AUTO_TEST_CASE(future_proposed_pending_test) try {
    BOOST_REQUIRE(next_header_state.pending_finalizer_policy == prev.pending_finalizer_policy);
 } FC_LOG_AND_RETHROW();
 
-BOOST_AUTO_TEST_CASE(empty_proposed_pending_test) try {
+BOOST_AUTO_TEST_CASE(empty_proposed_or_pending_test) try {
    // When a policy is empty, nothing changes
-   block_header_state prev;
-   prev.proposed_finalizer_policies.clear(); // make it empty
-   prev.pending_finalizer_policy = std::nullopt;
 
+   block_header_state prev;
    block_header_state next_header_state;
+
    next_header_state.core.links.emplace_back(qc_link{.target_block_num = 1u}); // lib
+   prev.proposed_finalizer_policies.clear(); // empty proposed_finalizer_policies
+   prev.pending_finalizer_policy = std::nullopt;  // empty pending_finalizer_policy
 
    evaluate_finalizer_policies_for_promotion(prev, next_header_state);
 
@@ -53,66 +55,74 @@ BOOST_AUTO_TEST_CASE(empty_proposed_pending_test) try {
 } FC_LOG_AND_RETHROW();
 
 BOOST_AUTO_TEST_CASE(proposed_promotion_no_pending_1_test) try {
-   // A proposed policy is due to promote (block_num equal to LIB), no existing pending
-   block_header_state prev;
-   prev.proposed_finalizer_policies.emplace_back(std::make_pair(4u, nullptr)); // block_num 4
-   prev.pending_finalizer_policy = std::nullopt;
+   // Pre:  the only proposed finalizer policy's block_num equals to LIB and no pending exists.
+   // Post: the proposed finalizer policy becomes pending and `proposed_finalizer_policies` becomes empty.
 
+   block_header_state prev;
    block_header_state next_header_state;
+
    next_header_state.core.links.emplace_back(qc_link{.target_block_num = 4u}); // lib
+   next_header_state.header.previous = id_from_num(6u); // block_num 7, which is previous + 1
+   prev.proposed_finalizer_policies.emplace_back(std::make_pair(4u, nullptr)); // block_num 4
+   prev.pending_finalizer_policy = std::nullopt; // no existing pending
 
    evaluate_finalizer_policies_for_promotion(prev, next_header_state);
 
    BOOST_REQUIRE(next_header_state.proposed_finalizer_policies.empty());
-   BOOST_REQUIRE(next_header_state.pending_finalizer_policy.has_value());
+   BOOST_REQUIRE(next_header_state.pending_finalizer_policy->first == 7u); // block num when becoming active
 } FC_LOG_AND_RETHROW();
 
 BOOST_AUTO_TEST_CASE(proposed_promotion_no_pending_2_test) try {
-   // A proposed policy is due to promote (block_num less than LIB), no existing pending
+   // Pre:  the only proposed finalizer policy's block_num is less than LIB and no pending exists.
+   // Post: the proposed finalizer policy becomes pending and `proposed_finalizer_policies` becomes empty.
+
    block_header_state prev;
+   block_header_state next_header_state;
+
+   next_header_state.core.links.emplace_back(qc_link{.target_block_num = 5u}); // lib
+   next_header_state.header.previous = id_from_num(7u); // block_num 8, which is previous + 1
    prev.proposed_finalizer_policies.emplace_back(std::make_pair(4u, nullptr)); // block_num 4
    prev.pending_finalizer_policy = std::nullopt;
 
-   block_header_state next_header_state;
-   next_header_state.core.links.emplace_back(qc_link{.target_block_num = 5u}); // lib
-
    evaluate_finalizer_policies_for_promotion(prev, next_header_state);
 
    BOOST_REQUIRE(next_header_state.proposed_finalizer_policies.empty());
-   BOOST_REQUIRE(next_header_state.pending_finalizer_policy.has_value());
+   BOOST_REQUIRE(next_header_state.pending_finalizer_policy->first == 8u); // block num when becoming active
 } FC_LOG_AND_RETHROW();
 
 BOOST_AUTO_TEST_CASE(pending_promotion_no_proposed_test) try {
-   // Pending promotes to active, no existing proposed
-   block_header_state prev;
+   // Pre:  the pending finalizer policy block_num is less than LIB and no proposed exists.
+   // Post: the pending finalizer policy becomes active and `proposed_finalizer_policies` stays empty.
 
+   block_header_state prev;
+   block_header_state next_header_state;
+
+   next_header_state.core.links.emplace_back(qc_link{.target_block_num = 5u}); // lib
    std::vector<finalizer_authority> finalizers;
    finalizer_policy_ptr fin_policy = std::make_shared<finalizer_policy>(10u /*generation*/, 15u, finalizers);
-   prev.pending_finalizer_policy = std::make_pair(4u, fin_policy); // becoming active
-
-   block_header_state next_header_state;
-   next_header_state.header.previous = id_from_num(7u); // block_num, which is previous + 1 
-   next_header_state.core.links.emplace_back(qc_link{.target_block_num = 5u}); // lib
+   prev.pending_finalizer_policy = std::make_pair(4u, fin_policy); // generation is 10
 
    evaluate_finalizer_policies_for_promotion(prev, next_header_state);
 
    BOOST_REQUIRE(next_header_state.proposed_finalizer_policies.empty());
-   BOOST_REQUIRE(!next_header_state.pending_finalizer_policy.has_value()); // pending slot available
-   BOOST_REQUIRE(next_header_state.active_finalizer_policy->generation == 10u);
+   BOOST_REQUIRE(!next_header_state.pending_finalizer_policy.has_value()); // No pending
+   BOOST_REQUIRE(next_header_state.active_finalizer_policy->generation == 10u); // pending (generation 10) becoming active
 } FC_LOG_AND_RETHROW();
 
 BOOST_AUTO_TEST_CASE(proposed_to_pending_to_active_test) try {
-   // Pending promotes to active, and proposed to pending
+   // Pre:  both proposed and pending finalizer policies block_nums are less than LIB
+   // Post: the proposed promoted to pending, the pending promoted to active.
+
    block_header_state prev;
+   block_header_state next_header_state;
+
+   next_header_state.header.previous = id_from_num(7u); // block_num 8, which is previous + 1
+   next_header_state.core.links.emplace_back(qc_link{.target_block_num = 5u}); // lib
    prev.proposed_finalizer_policies.emplace_back(std::make_pair(4u, nullptr)); // becoming pending
 
    std::vector<finalizer_authority> finalizers;
    finalizer_policy_ptr fin_policy = std::make_shared<finalizer_policy>(10u /*generation*/, 15u, finalizers);
    prev.pending_finalizer_policy = std::make_pair(4u, fin_policy); // becoming active
-
-   block_header_state next_header_state;
-   next_header_state.header.previous = id_from_num(7u); // block_num, which is previous + 1 
-   next_header_state.core.links.emplace_back(qc_link{.target_block_num = 5u}); // lib
 
    evaluate_finalizer_policies_for_promotion(prev, next_header_state);
 
@@ -122,20 +132,27 @@ BOOST_AUTO_TEST_CASE(proposed_to_pending_to_active_test) try {
 } FC_LOG_AND_RETHROW();
 
 BOOST_AUTO_TEST_CASE(proposed_promotion_pending_promotion_test) try {
-   // Pending promotes to active, highest proposed policy to pending, old proposed policies removed
+   // Pre:  pending block num less than LIB; 2 proposed block nums less than LIB,
+   //       1 proposed block num equal to LIB, 1 greater.
+   // Post: 2 proposed garbage collected, 1 becoming pending, 1 kept as proposed;
+   //       the pending becomes active.
+
    block_header_state prev;
+   block_header_state next_header_state;
+
+   next_header_state.header.previous = id_from_num(7u); // block_num, which is previous + 1
+   next_header_state.core.links.emplace_back(qc_link{.target_block_num = 5u}); // lib
+
    prev.proposed_finalizer_policies = {
      {3u, nullptr},  // garbage collected
      {4u, nullptr},  // garbage collected
      {5u, nullptr},  // becoming pending
      {6u, nullptr}   // kept as proposed
    };
-
    std::vector<finalizer_authority> finalizers;
    finalizer_policy_ptr fin_policy = std::make_shared<finalizer_policy>(10u /*generation*/, 15u, finalizers);
    prev.pending_finalizer_policy = std::make_pair(4u, fin_policy); // becoming active
 
-   block_header_state next_header_state;
    next_header_state.header.previous = id_from_num(7u); // block_num, which is previous + 1 
    next_header_state.core.links.emplace_back(qc_link{.target_block_num = 5u}); // lib
 
@@ -148,20 +165,23 @@ BOOST_AUTO_TEST_CASE(proposed_promotion_pending_promotion_test) try {
 } FC_LOG_AND_RETHROW();
 
 BOOST_AUTO_TEST_CASE(proposed_promotion_no_pending_promotion_test) try {
-   // No existing pending, highest proposed policy to pending, old proposed policies removed
+   // Pre:  no existing pending;
+   //       2 proposed block nums less than LIB, 1 equal, 1 greater.
+   // Post: proposed less than LIB garbage collected, proposed equal to LIB becoming pending,
+   //       proposed greater than LIB kept as proposed;
+
    block_header_state prev;
+   block_header_state next_header_state;
+
+   next_header_state.header.previous = id_from_num(7u); // block_num, which is previous + 1
+   next_header_state.core.links.emplace_back(qc_link{.target_block_num = 5u}); // lib
+
    prev.proposed_finalizer_policies = {
      {3u, nullptr},  // garbage collected
      {4u, nullptr},  // garbage collected
      {5u, nullptr},  // becoming pending
      {6u, nullptr}   // kept as proposed
    };
-
-   std::vector<finalizer_authority> finalizers;
-
-   block_header_state next_header_state;
-   next_header_state.header.previous = id_from_num(7u); // block_num, which is previous + 1 
-   next_header_state.core.links.emplace_back(qc_link{.target_block_num = 5u}); // lib
 
    evaluate_finalizer_policies_for_promotion(prev, next_header_state);
 
@@ -171,21 +191,24 @@ BOOST_AUTO_TEST_CASE(proposed_promotion_no_pending_promotion_test) try {
 } FC_LOG_AND_RETHROW();
 
 BOOST_AUTO_TEST_CASE(no_proposed_promotion_no_pending_promotion_1_test) try {
-   // Pending not due for promotion, no proposed to be promoted, old proposed policies removed
+   // Pre:  pending block number greater than LIB;
+   //       2 proposed block nums less than LIB, 1 equal, 1 greater.
+   // Post: Pending not due for promotion;
+   //       As no pending slot is open, no proposed is promoted to pending;
+   //       proposed less than LIB garbage collected, proposed equal and greater
+   //       than LIB kept as proposed;
+
    block_header_state prev;
+   block_header_state next_header_state;
+
+   next_header_state.core.links.emplace_back(qc_link{.target_block_num = 5u}); // lib
    prev.proposed_finalizer_policies = {
      {3u, nullptr},  // garbage collected
      {4u, nullptr},  // garbage collected
      {5u, nullptr},  // kept as proposed
      {6u, nullptr}   // kept as proposed
    };
-
-   std::vector<finalizer_authority> finalizers;
    prev.pending_finalizer_policy = std::make_pair(6u, nullptr); // does NOT become active as block num (6) greater than lib (5)
-
-   block_header_state next_header_state;
-   next_header_state.header.previous = id_from_num(7u); // block_num, which is previous + 1 
-   next_header_state.core.links.emplace_back(qc_link{.target_block_num = 5u}); // lib
 
    evaluate_finalizer_policies_for_promotion(prev, next_header_state);
 
@@ -196,19 +219,19 @@ BOOST_AUTO_TEST_CASE(no_proposed_promotion_no_pending_promotion_1_test) try {
 } FC_LOG_AND_RETHROW();
 
 BOOST_AUTO_TEST_CASE(no_proposed_promotion_no_pending_promotion_2_test) try {
-   // No existing pending, no proposed policy due for promotion
+   // Pre:  no pending exists; all proposed block nums greater than LIB
+   // Post: pending slot stays open; proposed not due for promotion
+
    block_header_state prev;
+   block_header_state next_header_state;
+
+   next_header_state.core.links.emplace_back(qc_link{.target_block_num = 5u}); // lib
    prev.proposed_finalizer_policies = {
      {6u, nullptr},  // greater than lib, kept as proposed
      {7u, nullptr},  // kept as proposed
      {8u, nullptr},  // kept as proposed
      {9u, nullptr}   // kept as proposed
    };
-
-   std::vector<finalizer_authority> finalizers;
-
-   block_header_state next_header_state;
-   next_header_state.core.links.emplace_back(qc_link{.target_block_num = 5u}); // lib
 
    evaluate_finalizer_policies_for_promotion(prev, next_header_state);
 
@@ -217,24 +240,30 @@ BOOST_AUTO_TEST_CASE(no_proposed_promotion_no_pending_promotion_2_test) try {
    BOOST_REQUIRE(next_header_state.proposed_finalizer_policies[1].first == 7u);
    BOOST_REQUIRE(next_header_state.proposed_finalizer_policies[2].first == 8u);
    BOOST_REQUIRE(next_header_state.proposed_finalizer_policies[3].first == 9u);
+   BOOST_REQUIRE(!next_header_state.pending_finalizer_policy.has_value()); // No pending
 } FC_LOG_AND_RETHROW();
 
 BOOST_AUTO_TEST_CASE(no_proposed_policies_left_test) try {
-   // Pending promotes to active, highest proposed policy to pending, old proposed policies removed
+   // Pre:  pending block number less than LIB;
+   //       2 proposed block nums less than LIB, 1 equal to LIB
+   // Post: pending promoted to active;
+   //       proposed less than LIB garbage collected, proposed equal to LIB becoming pending,
+
    block_header_state prev;
+   block_header_state next_header_state;
+
+   next_header_state.header.previous = id_from_num(7u); // block_num, which is previous + 1
+   next_header_state.core.links.emplace_back(qc_link{.target_block_num = 5u}); // lib
+
    prev.proposed_finalizer_policies = {
      {3u, nullptr},  // garbage collected
      {4u, nullptr},  // garbage collected
      {5u, nullptr},  // becoming pending
    };
-
    std::vector<finalizer_authority> finalizers;
    finalizer_policy_ptr fin_policy = std::make_shared<finalizer_policy>(10u /*generation*/, 15u, finalizers);
    prev.pending_finalizer_policy = std::make_pair(4u, fin_policy); // becoming active
 
-   block_header_state next_header_state;
-   next_header_state.header.previous = id_from_num(7u); // block_num, which is previous + 1 
-   next_header_state.core.links.emplace_back(qc_link{.target_block_num = 5u}); // lib
 
    evaluate_finalizer_policies_for_promotion(prev, next_header_state);
 
@@ -244,8 +273,16 @@ BOOST_AUTO_TEST_CASE(no_proposed_policies_left_test) try {
 } FC_LOG_AND_RETHROW();
 
 BOOST_AUTO_TEST_CASE(pending_promoted_proposed_not_promoted_test) try {
-   // Pending promotes to active, no proposed due for promotion: exiting proposed kept
+   // Pre:  pending block number less than LIB; all proposed block nums greater than LIB
+   // Post: pending promoted to active;
+   //       all proposed stay as proposed.
+
    block_header_state prev;
+   block_header_state next_header_state;
+
+   next_header_state.header.previous = id_from_num(7u); // block_num, which is previous + 1
+   next_header_state.core.links.emplace_back(qc_link{.target_block_num = 5u}); // lib
+   // Pending promotes to active, no proposed due for promotion: exiting proposed kept
    prev.proposed_finalizer_policies = {
      {6u, nullptr},  // kept as proposed
      {7u, nullptr},  // kept as proposed
@@ -256,10 +293,6 @@ BOOST_AUTO_TEST_CASE(pending_promoted_proposed_not_promoted_test) try {
    std::vector<finalizer_authority> finalizers;
    finalizer_policy_ptr fin_policy = std::make_shared<finalizer_policy>(10u /*generation*/, 15u, finalizers);
    prev.pending_finalizer_policy = std::make_pair(4u, fin_policy); // becoming active
-
-   block_header_state next_header_state;
-   next_header_state.header.previous = id_from_num(7u); // block_num, which is previous + 1 
-   next_header_state.core.links.emplace_back(qc_link{.target_block_num = 5u}); // lib
 
    evaluate_finalizer_policies_for_promotion(prev, next_header_state);
 
