98689b967f228de6d3f74d75edcce22b1413c733
Guillaume Babin-Tremblay <gbabint@msn.com>
2024-07-08 08:20:11 -0600

    Added comment regarding bitset reverse byte order

--- unittests/svnn_ibc_tests.cpp
+++ unittests/svnn_ibc_tests.cpp
@@ -623,7 +623,7 @@ BOOST_AUTO_TEST_SUITE(svnn_ibc)
       );
 
       chain.push_action("ibc"_n, "testbitset"_n, "ibc"_n, mvo()
-         ("bitset_string", "30")
+         ("bitset_string", "30") //bitset bytes are reversed, so we do the same to test
          ("bitset_vector", bitset_2)
          ("finalizers_count", 3)
       );
