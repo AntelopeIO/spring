8b4fd9005873b201bf1cc5cbd68e51a614964e59
Lin Huang <lin.huang@eosnetwork.com>
2024-07-10 10:49:05 -0400

    Define finalizer_authority_with_string_key which uses string formatted public key

--- libraries/chain/include/eosio/chain/finality/finalizer_authority.hpp
+++ libraries/chain/include/eosio/chain/finality/finalizer_authority.hpp
@@ -14,6 +14,19 @@ namespace eosio::chain {
       auto operator<=>(const finalizer_authority&) const = default;
    };
 
+   // This is used by SHiP and Deepmind which require public keys in string format.
+   struct finalizer_authority_with_string_key {
+      std::string  description;
+      uint64_t     weight = 0;
+      std::string  public_key;
+
+      finalizer_authority_with_string_key() = default;
+      finalizer_authority_with_string_key(const finalizer_authority& input)
+         : description(input.description)
+         , weight(input.weight)
+         , public_key(input.public_key.to_string()) {}
+   };
 } /// eosio::chain
 
 FC_REFLECT( eosio::chain::finalizer_authority, (description)(weight)(public_key) )
+FC_REFLECT( eosio::chain::finalizer_authority_with_string_key, (description)(weight)(public_key) )
