e8836db700650f05cead29f2d30a55d3aa2c303d
Kevin Heifner <heifnerk@objectcomputing.com>
2024-07-16 08:14:14 -0500

    GH-363 Update test

--- tests/block_log.cpp
+++ tests/block_log.cpp
@@ -587,7 +587,7 @@ BOOST_DATA_TEST_CASE(no_block_log_basic_nongenesis, bdata::xrange(2) * bdata::xr
 void no_block_log_public_functions_test( block_log_fixture& t) {
    BOOST_REQUIRE_NO_THROW(t.log->flush());
    BOOST_REQUIRE(t.log->read_block_by_num(1) == nullptr);
-   BOOST_REQUIRE(t.log->read_block_id_by_num(1) == eosio::chain::block_id_type{});
+   BOOST_REQUIRE(!t.log->read_block_id_by_num(1));
    BOOST_REQUIRE(t.log->get_block_pos(1) == eosio::chain::block_log::npos);
    BOOST_REQUIRE(t.log->read_head() == nullptr);
 }
