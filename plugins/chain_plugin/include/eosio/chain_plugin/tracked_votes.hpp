07a6e5ca55c20fa0ec173b8ffcafed77d0df306f
Lin Huang <lin.huang@eosnetwork.com>
2024-07-31 13:22:06 -0400

    Implement tracked_votes cache

---
+++ plugins/chain_plugin/include/eosio/chain_plugin/tracked_votes.hpp
@@ -0,0 +1,49 @@
+#pragma once
+#include <eosio/chain/types.hpp>
+#include <eosio/chain/block.hpp>
+#include <eosio/chain/controller.hpp>
+
+namespace eosio::chain_apis {
+   /**
+    * This class manages the ephemeral data that is needed by `get_finalizers_info` RPC call.
+    * There is no persistence and the cache is recreated when the class is instantiated
+    * based on the current state of the chain.
+    */
+   class tracked_votes {
+   public:
+
+      /**
+       * Instantiate a new tracked votes cache from the given chain controller
+       * The caller is expected to manage lifetimes such that this controller reference does not go stale
+       * for the life of the tracked votes cache
+       * @param chain - controller to read data from
+       */
+      tracked_votes( const class eosio::chain::controller& chain );
+      ~tracked_votes();
+
+      /**
+       * vote information for a given finalizer.
+       */
+      struct vote_info {
+         std::string          public_key;
+         std::string          description;
+         bool                 is_vote_strong{false};
+         chain::block_id_type voted_block_id;
+         uint32_t             voted_block_num{0};
+         fc::time_point       voted_block_timestamp;
+      };
+
+      // Called on accepted_block signal. Retrieve vote information from
+      // QC in the block and store it in last_votes.
+      void on_accepted_block(const chain::signed_block_ptr& block, const chain::block_id_type& id);
+
+      // Returns last vote information by a given finalizer
+      std::optional<vote_info> get_last_vote_info(const std::string& finalizer_pub_key) const;
+
+   private:
+      std::unique_ptr<struct tracked_votes_impl> _impl;
+   };
+
+}
+
+FC_REFLECT( eosio::chain_apis::tracked_votes::vote_info, (public_key)(description)(is_vote_strong)(voted_block_id)(voted_block_num)(voted_block_timestamp))
---
+++ plugins/chain_plugin/tracked_votes.cpp
@@ -0,0 +1,81 @@
+#include <eosio/chain_plugin/tracked_votes.hpp>
+
+#include <shared_mutex>
+
+using namespace eosio;
+using namespace eosio::chain::literals;
+
+namespace eosio::chain_apis {
+   /**
+    * Implementation details of the last tracked cache
+    */
+   struct tracked_votes_impl {
+      tracked_votes_impl(const chain::controller& controller)
+      :controller(controller)
+      {}
+
+      // Cache to store last vote information for each known finalizer.
+      // A map of finalizer public key --> vote info.
+      std::unordered_map<std::string, tracked_votes::vote_info> last_votes;
+
+      // A handle to the controller.
+      const chain::controller& controller;
+
+      // Called on accepted_block signal. Retrieve vote information from
+      // QC in the block and store it in last_votes.
+      void on_accepted_block( const chain::signed_block_ptr& block, const chain::block_id_type& id ) {
+         if (!block->contains_extension(chain::quorum_certificate_extension::extension_id())) {
+            return;
+         }
+
+         // Retrieve vote information from QC
+         const auto& qc_ext = block->extract_extension<chain::quorum_certificate_extension>();
+         chain::qc_vote_metrics_t vm = controller.vote_metrics(id, qc_ext.qc);
+
+         auto track_votes = [&](const chain::qc_vote_metrics_t::fin_auth_set_t& finalizers, bool is_strong) {
+            for (auto& f: finalizers) {
+               assert(f);
+
+               tracked_votes::vote_info v_info {
+                  .public_key            = f->public_key.to_string(),
+                  .description           = f->description,
+                  .is_vote_strong        = true,
+                  .voted_block_id        = vm.voted_block_id,
+                  .voted_block_num       = chain::block_header::num_from_id(vm.voted_block_id),
+                  .voted_block_timestamp = vm.voted_block_timestamp
+               };
+
+               last_votes.emplace(f->public_key.to_string(), v_info); // track the voting information for the finalizer
+            }
+         };
+
+         track_votes(vm.strong_votes, true);
+         track_votes(vm.weak_votes, false);
+      }
+
+      // Returns last vote information by a given finalizer
+      std::optional<tracked_votes::vote_info> get_last_vote_info(const std::string& finalizer_pub_key) const {
+         auto it = last_votes.find(finalizer_pub_key);
+         if (it != last_votes.end()) {
+             return it->second;
+         }
+
+         return {};
+      }
+   }; // tracked_votes_impl
+
+   tracked_votes::tracked_votes( const chain::controller& controller )
+   :_impl(std::make_unique<tracked_votes_impl>(controller))
+   {
+   }
+
+   tracked_votes::~tracked_votes() = default;
+
+   void tracked_votes::on_accepted_block( const chain::signed_block_ptr& block, const chain::block_id_type& id ) {
+      _impl->on_accepted_block(block, id);
+   }
+
+   std::optional<tracked_votes::vote_info> tracked_votes::get_last_vote_info(const std::string& finalizer_pub_key) const {
+      return _impl->get_last_vote_info(finalizer_pub_key);
+   }
+} // namespace eosio::chain_apis
