5c7e9f6a6c175e80045405939cc87bccfb93fce9
greg7mdp <greg7mdp@gmail.com>
2024-08-02 11:12:54 -0400

    Switch order of arguments to `BOOST_REQUIRE_EQUAL`.

--- unittests/savanna_disaster_recovery.cpp
+++ unittests/savanna_disaster_recovery.cpp
@@ -10,10 +10,10 @@ BOOST_FIXTURE_TEST_CASE(node_goes_down, savanna_cluster::cluster_t) try {
    auto& A=_nodes[0]; auto& C=_nodes[2];
 
    C.close();                                                                  // shutdown node C
-   BOOST_REQUIRE_EQUAL(A.lib_advances_by([&]() { A.produce_blocks(4);  }), 4); // lib still advances with 3 finalizers
+   BOOST_REQUIRE_EQUAL(4, A.lib_advances_by([&]() { A.produce_blocks(4);  })); // lib still advances with 3 finalizers
    C.open();                                                                   // restart node C
    A.push_blocks_to(C);                                                        // propagate blocks A -> C
-   BOOST_REQUIRE_EQUAL(A.lib_advances_by([&]() { A.produce_blocks(4);  }), 4); // all 4 finalizers should be back voting
+   BOOST_REQUIRE_EQUAL(4, A.lib_advances_by([&]() { A.produce_blocks(4);  })); // all 4 finalizers should be back voting
    BOOST_REQUIRE(!C.is_head_missing_finalizer_votes());                        // let's make sure of that
 } FC_LOG_AND_RETHROW()
 
@@ -23,16 +23,16 @@ BOOST_FIXTURE_TEST_CASE(recover_killed_node_with_old_fsi, savanna_cluster::clust
    auto& A=_nodes[0]; auto& C=_nodes[2];
 
    auto fsi = C.save_fsi();
-   BOOST_REQUIRE_EQUAL(A.lib_advances_by([&]() { A.produce_blocks(2);  }), 2);
+   BOOST_REQUIRE_EQUAL(2, A.lib_advances_by([&]() { A.produce_blocks(2);  }));
    auto snapshot = C.snapshot();
-   BOOST_REQUIRE_EQUAL(A.lib_advances_by([&]() { A.produce_blocks(2);  }), 2);
+   BOOST_REQUIRE_EQUAL(2, A.lib_advances_by([&]() { A.produce_blocks(2);  }));
    C.close();
-   BOOST_REQUIRE_EQUAL(A.lib_advances_by([&]() { A.produce_blocks(2);  }), 2); // lib still advances with 3 finalizers
+   BOOST_REQUIRE_EQUAL(2, A.lib_advances_by([&]() { A.produce_blocks(2);  })); // lib still advances with 3 finalizers
    C.remove_state();
    C.overwrite_fsi(fsi);
    C.open_from_snapshot(snapshot);
    A.push_blocks_to(C);
-   BOOST_REQUIRE_EQUAL(A.lib_advances_by([&]() { A.produce_blocks(2);  }), 2); // all 4 finalizers should be back voting
+   BOOST_REQUIRE_EQUAL(2, A.lib_advances_by([&]() { A.produce_blocks(2);  })); // all 4 finalizers should be back voting
    BOOST_REQUIRE(!C.is_head_missing_finalizer_votes());                        // let's make sure of that
 } FC_LOG_AND_RETHROW()
 
@@ -40,16 +40,16 @@ BOOST_FIXTURE_TEST_CASE(recover_killed_node_with_old_fsi, savanna_cluster::clust
 BOOST_FIXTURE_TEST_CASE(recover_killed_node_with_deleted_fsi, savanna_cluster::cluster_t) try {
    auto& A=_nodes[0]; auto& C=_nodes[2];
 
-   BOOST_REQUIRE_EQUAL(A.lib_advances_by([&]() { A.produce_blocks(2);  }), 2);
+   BOOST_REQUIRE_EQUAL(2, A.lib_advances_by([&]() { A.produce_blocks(2);  }));
    auto snapshot = C.snapshot();
-   BOOST_REQUIRE_EQUAL(A.lib_advances_by([&]() { A.produce_blocks(2);  }), 2);
+   BOOST_REQUIRE_EQUAL(2, A.lib_advances_by([&]() { A.produce_blocks(2);  }));
    C.close();
-   BOOST_REQUIRE_EQUAL(A.lib_advances_by([&]() { A.produce_blocks(2);  }), 2); // lib still advances with 3 finalizers
+   BOOST_REQUIRE_EQUAL(2, A.lib_advances_by([&]() { A.produce_blocks(2);  })); // lib still advances with 3 finalizers
    C.remove_state();
    C.remove_fsi();
    C.open_from_snapshot(snapshot);
    A.push_blocks_to(C);
-   BOOST_REQUIRE_EQUAL(A.lib_advances_by([&]() { A.produce_blocks(2);  }), 2); // all 4 finalizers should be back voting
+   BOOST_REQUIRE_EQUAL(2, A.lib_advances_by([&]() { A.produce_blocks(2);  })); // all 4 finalizers should be back voting
    BOOST_REQUIRE(!C.is_head_missing_finalizer_votes());                        // let's make sure of that
 } FC_LOG_AND_RETHROW()
 
@@ -57,15 +57,15 @@ BOOST_FIXTURE_TEST_CASE(recover_killed_node_with_deleted_fsi, savanna_cluster::c
 BOOST_FIXTURE_TEST_CASE(recover_killed_node_while_retaining_fsi, savanna_cluster::cluster_t) try {
    auto& A=_nodes[0]; auto& C=_nodes[2];
 
-   BOOST_REQUIRE_EQUAL(A.lib_advances_by([&]() { A.produce_blocks(2);  }), 2);
+   BOOST_REQUIRE_EQUAL(2, A.lib_advances_by([&]() { A.produce_blocks(2);  }));
    auto snapshot = C.snapshot();
-   BOOST_REQUIRE_EQUAL(A.lib_advances_by([&]() { A.produce_blocks(2);  }), 2);
+   BOOST_REQUIRE_EQUAL(2, A.lib_advances_by([&]() { A.produce_blocks(2);  }));
    C.close();
-   BOOST_REQUIRE_EQUAL(A.lib_advances_by([&]() { A.produce_blocks(2);  }), 2); // lib still advances with 3 finalizers
+   BOOST_REQUIRE_EQUAL(2, A.lib_advances_by([&]() { A.produce_blocks(2);  })); // lib still advances with 3 finalizers
    C.remove_state();
    C.open_from_snapshot(snapshot);
    A.push_blocks_to(C);
-   BOOST_REQUIRE_EQUAL(A.lib_advances_by([&]() { A.produce_blocks(2);  }), 2); // all 4 finalizers should be back voting
+   BOOST_REQUIRE_EQUAL(2, A.lib_advances_by([&]() { A.produce_blocks(2);  })); // all 4 finalizers should be back voting
    BOOST_REQUIRE(!C.is_head_missing_finalizer_votes());                        // let's make sure of that
 } FC_LOG_AND_RETHROW()
 
@@ -80,10 +80,10 @@ BOOST_FIXTURE_TEST_CASE(nodes_go_down, savanna_cluster::cluster_t) try {
    std::array<savanna_cluster::node_t*, 3> failing_nodes { &B, &C, &D };
 
    for (auto& N : failing_nodes) N->close();
-   BOOST_REQUIRE_EQUAL(A.lib_advances_by([&]() { A.produce_blocks(4);  }), 1); // lib stalls with 3 finalizers down
+   BOOST_REQUIRE_EQUAL(1, A.lib_advances_by([&]() { A.produce_blocks(4);  })); // lib stalls with 3 finalizers down
    for (auto& N : failing_nodes) N->open();
    for (auto& N : failing_nodes) A.push_blocks_to(*N);
-   BOOST_REQUIRE_EQUAL(A.lib_advances_by([&]() { A.produce_blocks(4);  }), 7); // all 4 finalizers should be back voting
+   BOOST_REQUIRE_EQUAL(7, A.lib_advances_by([&]() { A.produce_blocks(4);  })); // all 4 finalizers should be back voting
    for (auto& N : failing_nodes) BOOST_REQUIRE(!N->is_head_missing_finalizer_votes());
 } FC_LOG_AND_RETHROW()
 
@@ -97,11 +97,11 @@ BOOST_FIXTURE_TEST_CASE(recover_killed_nodes_with_old_fsi, savanna_cluster::clus
    std::vector<std::string> snapshots;
 
    for (auto& N : failing_nodes) fsis.push_back(N->save_fsi());
-   BOOST_REQUIRE_EQUAL(A.lib_advances_by([&]() { A.produce_blocks(2);  }), 2);
+   BOOST_REQUIRE_EQUAL(2, A.lib_advances_by([&]() { A.produce_blocks(2);  }));
    for (auto& N : failing_nodes) snapshots.push_back(N->snapshot());
-   BOOST_REQUIRE_EQUAL(A.lib_advances_by([&]() { A.produce_blocks(2);  }), 2);
+   BOOST_REQUIRE_EQUAL(2, A.lib_advances_by([&]() { A.produce_blocks(2);  }));
    for (auto& N : failing_nodes) N->close();
-   BOOST_REQUIRE_EQUAL(A.lib_advances_by([&]() { A.produce_blocks(2);  }), 1); // lib stalls 3 finalizers down
+   BOOST_REQUIRE_EQUAL(1, A.lib_advances_by([&]() { A.produce_blocks(2);  })); // lib stalls 3 finalizers down
    size_t i = 0;
    for (auto& N : failing_nodes) {
       N->remove_state();
@@ -110,7 +110,7 @@ BOOST_FIXTURE_TEST_CASE(recover_killed_nodes_with_old_fsi, savanna_cluster::clus
       A.push_blocks_to(*N);
       ++i;
    }
-   BOOST_REQUIRE_EQUAL(A.lib_advances_by([&]() { A.produce_blocks(2);  }), 3); // all 4 finalizers should be back voting
+   BOOST_REQUIRE_EQUAL(3, A.lib_advances_by([&]() { A.produce_blocks(2);  })); // all 4 finalizers should be back voting
    for (auto& N : failing_nodes) BOOST_REQUIRE(!N->is_head_missing_finalizer_votes());
 } FC_LOG_AND_RETHROW()
 
@@ -121,11 +121,11 @@ BOOST_FIXTURE_TEST_CASE(recover_killed_nodes_with_deleted_fsi, savanna_cluster::
 
    std::vector<std::string> snapshots;
 
-   BOOST_REQUIRE_EQUAL(A.lib_advances_by([&]() { A.produce_blocks(2);  }), 2);
+   BOOST_REQUIRE_EQUAL(2, A.lib_advances_by([&]() { A.produce_blocks(2);  }));
    for (auto& N : failing_nodes) snapshots.push_back(N->snapshot());
-   BOOST_REQUIRE_EQUAL(A.lib_advances_by([&]() { A.produce_blocks(2);  }), 2);
+   BOOST_REQUIRE_EQUAL(2, A.lib_advances_by([&]() { A.produce_blocks(2);  }));
    for (auto& N : failing_nodes) N->close();
-   BOOST_REQUIRE_EQUAL(A.lib_advances_by([&]() { A.produce_blocks(2);  }), 1); // lib stalls 3 finalizers down
+   BOOST_REQUIRE_EQUAL(1, A.lib_advances_by([&]() { A.produce_blocks(2);  })); // lib stalls 3 finalizers down
    size_t i = 0;
    for (auto& N : failing_nodes) {
       N->remove_state();
@@ -134,7 +134,7 @@ BOOST_FIXTURE_TEST_CASE(recover_killed_nodes_with_deleted_fsi, savanna_cluster::
       A.push_blocks_to(*N);
       ++i;
    }
-   BOOST_REQUIRE_EQUAL(A.lib_advances_by([&]() { A.produce_blocks(2);  }), 3); // all 4 finalizers should be back voting
+   BOOST_REQUIRE_EQUAL(3, A.lib_advances_by([&]() { A.produce_blocks(2);  })); // all 4 finalizers should be back voting
    for (auto& N : failing_nodes) BOOST_REQUIRE(!N->is_head_missing_finalizer_votes());
 } FC_LOG_AND_RETHROW()
 
@@ -145,11 +145,11 @@ BOOST_FIXTURE_TEST_CASE(recover_killed_nodes_while_retaining_fsi, savanna_cluste
 
    std::vector<std::string> snapshots;
 
-   BOOST_REQUIRE_EQUAL(A.lib_advances_by([&]() { A.produce_blocks(2);  }), 2);
+   BOOST_REQUIRE_EQUAL(2, A.lib_advances_by([&]() { A.produce_blocks(2);  }));
    for (auto& N : failing_nodes) snapshots.push_back(N->snapshot());
-   BOOST_REQUIRE_EQUAL(A.lib_advances_by([&]() { A.produce_blocks(2);  }), 2);
+   BOOST_REQUIRE_EQUAL(2, A.lib_advances_by([&]() { A.produce_blocks(2);  }));
    for (auto& N : failing_nodes) N->close();
-   BOOST_REQUIRE_EQUAL(A.lib_advances_by([&]() { A.produce_blocks(2);  }), 1); // lib stalls 3 finalizers down
+   BOOST_REQUIRE_EQUAL(1, A.lib_advances_by([&]() { A.produce_blocks(2);  })); // lib stalls 3 finalizers down
    size_t i = 0;
    for (auto& N : failing_nodes) {
       N->remove_state();
@@ -157,7 +157,7 @@ BOOST_FIXTURE_TEST_CASE(recover_killed_nodes_while_retaining_fsi, savanna_cluste
       A.push_blocks_to(*N);
       ++i;
    }
-   BOOST_REQUIRE_EQUAL(A.lib_advances_by([&]() { A.produce_blocks(2);  }), 3); // all 4 finalizers should be back voting
+   BOOST_REQUIRE_EQUAL(3, A.lib_advances_by([&]() { A.produce_blocks(2);  })); // all 4 finalizers should be back voting
    for (auto& N : failing_nodes) BOOST_REQUIRE(!N->is_head_missing_finalizer_votes());
 } FC_LOG_AND_RETHROW()
 
@@ -170,7 +170,7 @@ BOOST_FIXTURE_TEST_CASE(all_nodes_shutdown_with_reversible_blocks_lost, savanna_
    auto& A=_nodes[0]; auto& B=_nodes[1]; auto& C=_nodes[2]; auto& D=_nodes[3];
    std::array<savanna_cluster::node_t*, 4> failing_nodes { &A, &B, &C, &D };
 
-   BOOST_REQUIRE_EQUAL(A.lib_advances_by([&]() { A.produce_blocks(2);  }), 2);
+   BOOST_REQUIRE_EQUAL(2, A.lib_advances_by([&]() { A.produce_blocks(2);  }));
 
    // take snapshot
    // -------------
@@ -183,7 +183,7 @@ BOOST_FIXTURE_TEST_CASE(all_nodes_shutdown_with_reversible_blocks_lost, savanna_
    for (auto& N : failing_nodes) BOOST_REQUIRE_EQUAL(N->head().id(), head_id);
 
    // produce two blocks so that lib catches up to snapshot's head
-   BOOST_REQUIRE_EQUAL(A.lib_advances_by([&]() { A.produce_blocks(2);  }), 2);
+   BOOST_REQUIRE_EQUAL(2, A.lib_advances_by([&]() { A.produce_blocks(2);  }));
 
    auto lib_id = A.lib_id;
    auto lib_num = A.lib_number;
@@ -195,7 +195,7 @@ BOOST_FIXTURE_TEST_CASE(all_nodes_shutdown_with_reversible_blocks_lost, savanna_
    // -------------------------------------------------------------------------------------
    const std::vector<size_t> partition {2, 3};
    set_partition(partition);
-   BOOST_REQUIRE_EQUAL(A.lib_advances_by([&]() { A.produce_blocks(2);  }), 1); // lib stalls with network partitioned
+   BOOST_REQUIRE_EQUAL(1, A.lib_advances_by([&]() { A.produce_blocks(2);  })); // lib stalls with network partitioned
 
    // remove network split
    // --------------------
