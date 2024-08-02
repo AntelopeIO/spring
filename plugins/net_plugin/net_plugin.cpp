83cc7cc425b14807d8c687544de596156372516c
Kevin Heifner <heifnerk@objectcomputing.com>
2024-08-02 17:04:49 -0500

    GH-459 Fix case where sync-fetch-span is 1

--- plugins/net_plugin/net_plugin.cpp
+++ plugins/net_plugin/net_plugin.cpp
@@ -2146,7 +2146,9 @@ namespace eosio {
 
       bool request_sent = false;
       if( sync_last_requested_num != sync_known_lib_num ) {
-         uint32_t start = std::min(sync_next_expected_num, chain_info.lib_num+1);
+         uint32_t start = sync_next_expected_num;
+         if (sync_req_span > 1)
+            start = std::min(start, chain_info.lib_num+1);
          uint32_t end = start + sync_req_span - 1;
          if( end > sync_known_lib_num )
             end = sync_known_lib_num;
