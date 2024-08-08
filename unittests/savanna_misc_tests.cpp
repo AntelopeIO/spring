c87f6dba461cd1813ccbaeb62d2d66459cf0e7cb
greg7mdp <greg7mdp@gmail.com>
2024-08-08 16:31:55 -0400

    Fix check for existing fork database.

---
+++ unittests/savanna_misc_tests.cpp
@@ -0,0 +1,43 @@
+#include "savanna_cluster.hpp"
+
+using namespace eosio::chain;
+using namespace eosio::testing;
+
+BOOST_AUTO_TEST_SUITE(savanna_misc_tests)
+
+// Verify that we can restart a node from a snapshot without state or blocks (reversible
+// or not)
+// ------------------------------------------------------------------------------------
+BOOST_FIXTURE_TEST_CASE(snapshot_startup_without_forkdb, savanna_cluster::cluster_t) try {
+   auto& A=_nodes[0]; auto& B=_nodes[1];
+
+   auto snapshot = B.snapshot();
+   A.produce_blocks(3);
+
+   B.close();
+   B.remove_reversible_data_and_blocks_log(); // remove blocks log *and* fork database
+   B.remove_state();
+   B.open_from_snapshot(snapshot);
+
+} FC_LOG_AND_RETHROW()
+
+// Verify that we cannot restart a node from a snapshot without state and blocks log,
+// but with a fork database
+// ------------------------------------------------------------------------------------
+BOOST_FIXTURE_TEST_CASE(snapshot_startup_with_forkdb, savanna_cluster::cluster_t) try {
+   auto& A=_nodes[0]; auto& B=_nodes[1];
+
+   auto snapshot = B.snapshot();
+   A.produce_blocks(3);
+
+   B.close();
+   B.remove_blocks_log(); // remove blocks log, but *not* fork database
+   B.remove_state();
+   BOOST_CHECK_EXCEPTION(B.open_from_snapshot(snapshot), snapshot_exception,
+                         fc_exception_message_is("When starting from a snapshot with no block log, we shouldn't have a fork database either"));
+
+} FC_LOG_AND_RETHROW()
+
+
+
+BOOST_AUTO_TEST_SUITE_END()
