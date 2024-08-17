ea2c4eb06acce1455e561b0ad2e6e5d248afe403
Matt Witherspoon <32485495+spoonincode@users.noreply.github.com>
2024-08-16 21:48:55 -0400

    get rid of LLVM 7.0.x ABI-mixup workaround

--- CMakeModules/CMakeDetermineASM-LLVMWARCompiler.cmake
+++
@@ -1,6 +0,0 @@
-# These trio of files implement a workaround for LLVM bug 39427
-
-set(ASM_DIALECT "-LLVMWAR")
-set(CMAKE_ASM${ASM_DIALECT}_COMPILER_INIT "g++")
-include(CMakeDetermineASMCompiler)
-set(ASM_DIALECT)
--- CMakeModules/CMakeTestASM-LLVMWARCompiler.cmake
+++
@@ -1,5 +0,0 @@
-# These trio of files implement a workaround for LLVM bug 39427
-
-set(ASM_DIALECT "-LLVMWAR")
-include(CMakeTestASMCompiler)
-set(ASM_DIALECT)
