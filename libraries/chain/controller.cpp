6f5408d6623df93e764b99c451890b89d2abbedc
Kevin Heifner <heifnerk@objectcomputing.com>
2024-08-20 15:14:38 -0500

    GH-529 Do not check terminate-at-block in create_block_handle

--- libraries/chain/controller.cpp
+++ libraries/chain/controller.cpp
@@ -3985,7 +3985,8 @@ struct controller_impl {
       }
 
       bool best_head = false;
-      if (!should_terminate(bsp->block_num() - 1)) { // we want to process terminate block, so -1 to allow it to pass
+      // want to process terminate block, so 1 to allow it to pass, but still check max-reversible-blocks
+      if (!should_terminate(1)) {
          best_head = forkdb.add(bsp, ignore_duplicate_t::yes);
          if constexpr (savanna_mode)
             vote_processor.notify_new_block(async_aggregation);
