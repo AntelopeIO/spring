27ade97dfe8c015af484cb6fcf923f029492a448
Lin Huang <lin.huang@eosnetwork.com>
2024-08-31 15:30:07 -0400

    Add paused() to Node class in TestHarness

--- tests/TestHarness/Node.py
+++ tests/TestHarness/Node.py
@@ -273,6 +273,53 @@ class Node(Transactions):
             f"Waited for {time.perf_counter()-start} sec but never found producer: {producer}. Started with {initialProducer} and ended with {self.getInfo()['head_block_producer']}"
         return found
 
+    # returns True if the node has paused production.
+    def paused(self):
+        # Cannot use producer_plugin's paused() endpoint as it does not
+        # include paused due to max-reversible-blocks exceeded.
+        # The idea is to find the expected start block of node's producer's
+        # next round. If that block is not produced, it means block production
+        # on the node is paused.
+
+        scheduled_producers = []
+        schedule = self.processUrllibRequest("chain", "get_producer_schedule")
+        for prod in schedule["payload"]["active"]["producers"]:
+            scheduled_producers.append(prod["producer_name"])
+        if Utils.Debug: Utils.Print(f'scheduled_producers {scheduled_producers}')
+
+        self.getInfo()
+        headBlockNum=self.lastRetrievedHeadBlockNum
+        headBlockProducer=self.lastRetrievedHeadBlockProducer
+        blocksRemainedCurrRound = 12 - headBlockNum%12 - 1
+        if Utils.Debug: Utils.Print(f'headBlockNum {headBlockNum}, headBlockProducer {headBlockProducer}, blocksRemainedCurrRound {blocksRemainedCurrRound}')
+
+        # find the positions of currProducerPos and nodeProducer in the schedule
+        currProducerPos=0
+        nodeProducerPos=0
+        for i in range(0, len(scheduled_producers)):
+            if scheduled_producers[i] == headBlockProducer:
+                currProducerPos=i
+            if scheduled_producers[i] == self.producerName:
+                nodeProducerPos=i
+
+        # find the block number of the node producer's next scheduled round
+        blocksToNextScheduledRound = 0
+        if currProducerPos < nodeProducerPos:
+            # nodeProducerPos - currProducerPos - 1 is the number of producers
+            # from current producer to the node producer in the schedule
+            blocksToNextScheduledRound = (nodeProducerPos - currProducerPos - 1) * 12 + blocksRemainedCurrRound
+        else:
+            # nodeProducerPos is the number of producers before node producer in the schedule
+            # len(scheduled_producers) - currProducerPos - 1 is the number
+            # of producers after node producer in the schedule
+            blocksToNextScheduledRound = (nodeProducerPos + (len(scheduled_producers)  - currProducerPos - 1)) * 12 + blocksRemainedCurrRound
+
+        nextScheduledRoundBlockNum=headBlockNum + blocksToNextScheduledRound + 1
+        timeout=blocksToNextScheduledRound/2 + 3 # leave 3 seconds for avoid timing issues
+        if Utils.Debug: Utils.Print(f'blocksToNextScheduledRound {blocksToNextScheduledRound}, nextScheduledRoundBlockNum {nextScheduledRoundBlockNum}, timeout {timeout}')
+
+        return not self.waitForBlock(nextScheduledRoundBlockNum, timeout=timeout)
+
     def killNodeOnProducer(self, producer, whereInSequence, blockType=BlockType.head, silentErrors=True, exitOnError=False, exitMsg=None, returnType=ReturnType.json):
         assert(isinstance(producer, str))
         assert(isinstance(whereInSequence, int))
