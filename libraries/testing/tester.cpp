9120c4876df1f0aa18d8662d490596312966b539
greg7mdp <greg7mdp@gmail.com>
2024-09-20 16:35:28 -0400

    Fix merge issue

--- libraries/testing/tester.cpp
+++ libraries/testing/tester.cpp
@@ -374,7 +374,7 @@ namespace eosio::testing {
       [[maybe_unused]] auto block_start_connection = control->block_start().connect([](block_num_type num) {
          // only block number is signaled, in forking tests will get the same block number more than once.
       });
-      [[maybe_unused]] auto accepted_block_header_connection = control->accepted_block_header().connect([this](const block_signal_params& t) {
+      [[maybe_unused]] auto accepted_block_header_connection = control->accepted_block_header().connect([&](const block_signal_params& t) {
             [[maybe_unused]] const auto& [block, id] = t;
             assert(block);
             assert(_check_signal(id, block_signal::accepted_block_header));
