aacff60ff1f894820ddea989e9e9c51678d238ef
greg7mdp <greg7mdp@gmail.com>
2024-10-18 15:58:59 -0400

    Use correct `get_block_info` API.

--- tests/PerformanceHarness/performance_test_basic.py
+++ tests/PerformanceHarness/performance_test_basic.py
@@ -339,7 +339,7 @@ class PerformanceTestBasic:
         emptyBlocks = 0
         while emptyBlocks < numEmptyToWaitOn:
             headBlock = node.getHeadBlockNum()
-            block = node.node.processUrllibRequest("chain", "get_block", {"block_num_or_id":headBlock}, silentErrors=False, exitOnError=True)
+            block = node.node.processUrllibRequest("chain", "get_block_info", {"block_num":headBlock}, silentErrors=False, exitOnError=True)
             node.waitForHeadToAdvance()
             if block['payload']['transaction_mroot'] == "0000000000000000000000000000000000000000000000000000000000000000":
                 emptyBlocks += 1
