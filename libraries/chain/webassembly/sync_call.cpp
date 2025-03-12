755fdba6f855756e340efffe8818feced63774c1
Lin Huang <lin.huang@eosnetwork.com>
2025-03-12 14:01:12 -0400

    Update host function call to use actual apply_conetxt's execute_sync_call

--- libraries/chain/webassembly/sync_call.cpp
+++ libraries/chain/webassembly/sync_call.cpp
@@ -1,8 +1,9 @@
 #include <eosio/chain/webassembly/interface.hpp>
+#include <eosio/chain/apply_context.hpp>
 
 namespace eosio { namespace chain { namespace webassembly {
-   void interface::call(name /* receiver */, uint64_t /* flags */, span<const char> /* data */) {
-      ;
+   void interface::call(name receiver, uint64_t flags, std::span<const char> data) {
+      context.execute_sync_call(receiver, flags, data);
    }
 
    uint32_t interface::get_call_data(span<char> /* memory */) const {
