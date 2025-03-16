318d440e9a8279df2ca491af899d031fca5d1da5
Lin Huang <lin.huang@eosnetwork.com>
2025-03-16 15:26:11 -0400

    Print out the value of MAX_SIZE_OF_BYTE_ARRAYS for EOS_ASSERT of max_sync_call_data_size <= MAX_SIZE_OF_BYTE_ARRAYS

--- libraries/chain/chain_config.cpp
+++ libraries/chain/chain_config.cpp
@@ -42,7 +42,8 @@ namespace eosio { namespace chain {
 void chain_config_v1::validate() const {
    chain_config_v0::validate();
    EOS_ASSERT( max_action_return_value_size <= MAX_SIZE_OF_BYTE_ARRAYS, action_validate_exception,
-               "max action return value size should be less than MAX_SIZE_OF_BYTE_ARRAYS" );
+               "max action return value size should be less than ${max} (MAX_SIZE_OF_BYTE_ARRAYS)",
+               ("max", MAX_SIZE_OF_BYTE_ARRAYS));
 }
 
 void chain_config_v2::validate() const {
@@ -50,7 +51,8 @@ void chain_config_v2::validate() const {
    EOS_ASSERT( 1 <= max_sync_call_depth, action_validate_exception,
                "max sync call depth should be at least 1" );
    EOS_ASSERT( max_sync_call_data_size <= MAX_SIZE_OF_BYTE_ARRAYS, action_validate_exception,
-               "max ssync call data size should be less than MAX_SIZE_OF_BYTE_ARRAYS" );
+               "max sync call data size should be less than ${max} (MAX_SIZE_OF_BYTE_ARRAYS)",
+               ("max", MAX_SIZE_OF_BYTE_ARRAYS));
 }
 
 bool config_entry_validator::operator()(uint32_t id) const {
