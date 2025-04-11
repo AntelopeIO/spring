ec988fa11f9eb52048d6fa6e0f141ab1263d2164
Kevin Heifner <heifnerk@objectcomputing.com>
2025-04-11 12:59:20 -0500

    GH-1209 Add comment

--- tests/p2p_no_blocks_test.py
+++ tests/p2p_no_blocks_test.py
@@ -94,6 +94,9 @@ try:
     transferAmount="50.0000 {0}".format(CORE_SYMBOL)
     trx=nonProdNode01.transferFunds(cluster.eosioAccount, cluster.defproduceraAccount, transferAmount, dontSend=True)
 
+    Print("Sync past transfer reference block")
+    # Make sure node2 and node3 have trx reference block before killing bios node otherwise the trx will fail
+    # because the reference block is not available.
     head = nonProdNode01.getHeadBlockNum()
     assert noBlocks02.waitForBlock(headBlockNum), "node02 did not get block before bios shutdown"
     assert noBlocks03.waitForBlock(headBlockNum), "node03 did not get block before bios shutdown"
