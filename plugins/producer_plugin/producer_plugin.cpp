b5949aae9e5c97947e0d9cdb5b988a9af555862e
Kevin Heifner <heifnerk@objectcomputing.com>
2025-04-16 06:41:13 -0500

    GH-1170 remove duplicate code

--- plugins/producer_plugin/producer_plugin.cpp
+++ plugins/producer_plugin/producer_plugin.cpp
@@ -1217,6 +1217,11 @@ public:
 
    bool in_producing_mode()   const { return _pending_block_mode == pending_block_mode::producing; }
    bool in_speculating_mode() const { return _pending_block_mode == pending_block_mode::speculating; }
+
+   void interrupt_transaction() {
+      if (_is_savanna_active) // interrupt during transition causes issues, so only allow after transition
+         chain_plug->chain().interrupt_transaction();
+   }
 };
 
 void new_chain_banner(const eosio::chain::controller& db)
@@ -2844,8 +2849,7 @@ void producer_plugin_impl::schedule_production_loop() {
       // we failed to start a block, so try again later?
       _timer.async_wait([this, cid = ++_timer_corelation_id](const boost::system::error_code& ec) {
          if (ec != boost::asio::error::operation_aborted && cid == _timer_corelation_id) {
-            if (_is_savanna_active) // interrupt during transition causes issues, so only allow after transition
-               chain_plug->chain().interrupt_transaction();
+            interrupt_transaction();
             app().executor().post(priority::high, exec_queue::read_write, [this]() {
                schedule_production_loop();
             });
@@ -2931,8 +2935,7 @@ void producer_plugin_impl::schedule_delayed_production_loop(const std::weak_ptr<
       _timer.expires_at(epoch + boost::posix_time::microseconds(wake_up_time->time_since_epoch().count()));
       _timer.async_wait([this, cid = ++_timer_corelation_id](const boost::system::error_code& ec) {
          if (ec != boost::asio::error::operation_aborted && cid == _timer_corelation_id) {
-            if (_is_savanna_active) // interrupt during transition causes issues, so only allow after transition
-               chain_plug->chain().interrupt_transaction();
+            interrupt_transaction();
             app().executor().post(priority::high, exec_queue::read_write, [this]() {
                schedule_production_loop();
             });
@@ -3055,10 +3058,10 @@ void producer_plugin::received_block(uint32_t block_num, chain::fork_db_add_t fo
    if (my->_is_savanna_active) { // interrupt during transition causes issues, so only allow after transition
       if (fork_db_add_result == fork_db_add_t::appended_to_head) {
          fc_tlog(_log, "new head block received, interrupting trx");
-         my->chain_plug->chain().interrupt_transaction();
+         my->interrupt_transaction();
       } else if (fork_db_add_result == fork_db_add_t::fork_switch) {
          fc_ilog(_log, "new best fork received, interrupting trx");
-         my->chain_plug->chain().interrupt_transaction();
+         my->interrupt_transaction();
       }
    }
 }
@@ -3068,8 +3071,7 @@ void producer_plugin::interrupt() {
    fc_ilog(_log, "interrupt");
    app().executor().stop(); // shutdown any blocking read_only_execution_task
    my->interrupt_read_only();
-   if (my->_is_savanna_active) // interrupt during transition causes issues, so only allow after transition
-      my->chain_plug->chain().interrupt_transaction();
+   my->interrupt_transaction();
 }
 
 void producer_plugin_impl::interrupt_read_only() {
