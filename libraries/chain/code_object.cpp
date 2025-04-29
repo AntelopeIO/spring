fc69fd0ce45bb01bfcdeaa51e42a09b3038053fc
Lin Huang <lin.huang@eosnetwork.com>
2025-04-29 09:54:58 -0400

    remove unnecessary static_assert for FC support of reflector_init

--- libraries/chain/code_object.cpp
+++ libraries/chain/code_object.cpp
@@ -5,9 +5,6 @@ namespace eosio { namespace chain {
 
 void code_object::reflector_init()
 {
-   static_assert(fc::raw::has_feature_reflector_init_on_unpacked_reflected_types,
-                 "code_object expects FC to support reflector_init");
-
    sync_call_supported = wasm_interface::is_sync_call_supported(code.data(), code.size());
 }
 
