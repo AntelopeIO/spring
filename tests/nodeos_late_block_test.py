156cfb2213443dc9d9573180bb7056bfdd169933
Kevin Heifner <heifnerk@objectcomputing.com>
2025-06-02 10:41:09 -0500

    GH-1492 Add test that receives blocks late while producing

---
+++ tests/nodeos_late_block_test.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+import os
+import shutil
+import signal
+import time
+from TestHarness import Cluster, TestHelper, Utils, WalletMgr
+from TestHarness.Node import BlockType
+
+###############################################################
+# nodeos_late_block_test
+#
+# Set up a cluster of 4 producer nodes so that 3 can reach consensus.
+# Node_00 - defproducera,b,c
+# Node_01 - defproducerd,e,f
+# Node_02 - defproducerg,h,i
+#  Node_04 - bridge between 2 & 3
+# Node_03 - defproducerj,k,l
+#
+# When Node_02 is producing shutdown Node_04 and bring it back up when Node_03 is producing.
+# Verify that Node_03 realizes it should switch over to fork other nodes have chosen.
+###############################################################
+
+Print=Utils.Print
+errorExit=Utils.errorExit
+
+args=TestHelper.parse_args({"-d","--keep-logs","--dump-error-details","-v","--leave-running","--unshared"})
+pnodes=4
+total_nodes=pnodes + 1
+delay=args.d
+debug=args.v
+dumpErrorDetails=args.dump_error_details
+
+Utils.Debug=debug
+testSuccessful=False
+
+cluster=Cluster(unshared=args.unshared, keepRunning=args.leave_running, keepLogs=args.keep_logs)
+walletMgr=WalletMgr(True, keepRunning=args.leave_running, keepLogs=args.keep_logs)
+
+try:
+    TestHelper.printSystemInfo("BEGIN")
+    cluster.setWalletMgr(walletMgr)
+
+    Print(f'producing nodes: {pnodes}, delay between nodes launch: {delay} second{"s" if delay != 1 else ""}')
+
+    Print("Stand up cluster")
+    # do not allow pause production to interfere with late block test
+    extraNodeosArgs=" --production-pause-vote-timeout-ms 0 "
+
+    if cluster.launch(pnodes=pnodes, totalNodes=total_nodes, extraNodeosArgs=extraNodeosArgs,
+                      topo="./tests/nodeos_late_block_test_shape.json", delay=delay, loadSystemContract=False,
+                      activateIF=True, signatureProviderForNonProducer=True) is False:
+        errorExit("Failed to stand up eos cluster.")
+
+    assert cluster.biosNode.getInfo(exitOnError=True)["head_block_producer"] != "eosio", "launch should have waited for production to change"
+    cluster.biosNode.kill(signal.SIGTERM)
+    cluster.waitOnClusterSync(blockAdvancing=5)
+
+    node3 = cluster.getNode(3)
+    node4 = cluster.getNode(4) # bridge between 2 & 3
+
+    node3.waitForAnyProducer(["defproducerg", "defproducerh"], exitOnError=True)
+    node4.kill(signal.SIGTERM)
+    assert not node4.verifyAlive(), "Node4 did not shutdown"
+
+    Print("Wait until Node_03 starts to produce its second round ")
+    node3.waitForProducer("defproducerk", exitOnError=True)
+
+    blockNum = node3.getHeadBlockNum()
+
+    Print("Relaunch bridge to connection Node_02 and Node_03")
+    node4.relaunch()
+
+    Print("Verify Node_03 fork switches even though it is producing")
+    node3.waitForNextBlock()
+    # Use blockNum+4 to give time for node4 to relaunch and production to be interrupted
+    # getBlockProducerByNum returns block according to best head out of the of forkdb
+    # call in a loop since getBlockProducerByNum uses get_info for waiting, but get_block which uses best head
+    defprod=None
+    while defprod is None:
+        defprod=node3.getBlockProducerByNum(blockNum+4, exitOnError=False)
+        time.sleep(0.25)
+    assert defprod == "defproducerg" or defprod == "defproducerh" or defprod == "defproduceri", \
+        f"Should have fork switched over to previous producers, instead {defprod}"
+    info_defprod = node3.getInfo(exitOnError=True)["head_block_producer"]
+    assert info_defprod == "defproducerg" or info_defprod == "defproducerh" or info_defprod == "defproduceri", \
+        f"Should have fork switched over to previous producers, instead: {info_defprod}"
+
+    Print("Wait until Node_00 to produce")
+    node3.waitForProducer("defproducera")
+
+    # verify still expected producer
+    defprod=node3.getBlockProducerByNum(blockNum+4)
+    assert defprod == "defproducerg" or defprod == "defproducerh" or defprod == "defproduceri", \
+        f"Should have fork switched over to previous producers, instead: {defprod}"
+
+    testSuccessful=True
+finally:
+    TestHelper.shutdown(cluster, walletMgr, testSuccessful=testSuccessful, dumpErrorDetails=dumpErrorDetails)
+
+exitCode = 0 if testSuccessful else 1
+exit(exitCode)
