969a59ada2e4c51f6a1d285cc7cb6d7a2ef1b1fc
Lin Huang <lin.huang@eosnetwork.com>
2025-06-11 09:56:52 -0400

    Call validating_tester constructor explicitly

--- unittests/sync_call_wasm_tests.cpp
+++ unittests/sync_call_wasm_tests.cpp
@@ -35,6 +35,8 @@ struct acct_code {
 
 // The first account in accounts vector must be the caller initiating a sync call
 struct call_tester : validating_tester {
+   call_tester() : validating_tester(){};
+
    void init(const std::vector<acct_code>& accounts) {
       for (auto i = 0u; i < accounts.size(); ++i) {
          create_account(accounts[i].name);
