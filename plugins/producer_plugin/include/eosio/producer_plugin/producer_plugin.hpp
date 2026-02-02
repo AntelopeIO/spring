Wire-Network/wire-sysio

Remove confusing size

plugins/producer_plugin/include/sysio/producer_plugin/producer_plugin.hpp
@@ -120,7 +120,6 @@ public:
    };
 
    struct get_unapplied_transactions_result {
-      size_t                     size = 0;
       size_t                     unapplied_size = 0;
       size_t                     queued_size = 0;
       std::vector<unapplied_trx> unapplied_trxs;
@@ -163,5 +162,5 @@ FC_REFLECT(sysio::producer_plugin::scheduled_protocol_feature_activations, (prot
 FC_REFLECT(sysio::producer_plugin::get_supported_protocol_features_params, (exclude_disabled)(exclude_unactivatable))
 FC_REFLECT(sysio::producer_plugin::get_unapplied_transactions_params, (lower_bound)(limit)(time_limit_ms))
 FC_REFLECT(sysio::producer_plugin::unapplied_trx, (trx_id)(expiration)(trx_type)(first_auth)(first_receiver)(first_action)(total_actions)(accounts_billing)(size))
-FC_REFLECT(sysio::producer_plugin::get_unapplied_transactions_result, (size)(unapplied_size)(queued_size)(unapplied_trxs)(queued_trxs)(more))
+FC_REFLECT(sysio::producer_plugin::get_unapplied_transactions_result, (unapplied_size)(queued_size)(unapplied_trxs)(queued_trxs)(more))
 FC_REFLECT(sysio::producer_plugin::pause_at_block_params, (block_num));

AntelopeIO/spring

plugins/producer_plugin/include/eosio/producer_plugin/producer_plugin.hpp
@@ -134,7 +134,6 @@ public:
    };
 
    struct get_unapplied_transactions_result {
-      size_t                     size = 0;
       size_t                     unapplied_size = 0;
       size_t                     queued_size = 0;
       std::vector<unapplied_trx> unapplied_trxs;
@@ -179,5 +178,5 @@ FC_REFLECT(eosio::producer_plugin::get_account_ram_corrections_params, (lower_bo
 FC_REFLECT(eosio::producer_plugin::get_account_ram_corrections_result, (rows)(more))
 FC_REFLECT(eosio::producer_plugin::get_unapplied_transactions_params, (lower_bound)(limit)(time_limit_ms))
 FC_REFLECT(eosio::producer_plugin::unapplied_trx, (trx_id)(expiration)(trx_type)(first_auth)(first_receiver)(first_action)(total_actions)(billed_cpu_time_us)(size))
-FC_REFLECT(eosio::producer_plugin::get_unapplied_transactions_result, (size)(unapplied_size)(queued_size)(unapplied_trxs)(queued_trxs)(more))
+FC_REFLECT(eosio::producer_plugin::get_unapplied_transactions_result, (unapplied_size)(queued_size)(unapplied_trxs)(queued_trxs)(more))
 FC_REFLECT(eosio::producer_plugin::pause_at_block_params, (block_num));